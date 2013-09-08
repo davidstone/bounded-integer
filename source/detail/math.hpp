913faa4c59c459b7461e91ad1f47e419e51cd01c
David Stone <david@doublewise.net>
2013-09-08 17:29:20 -0600

    Fixed overview comment on what math.hpp is.

--- source/detail/math.hpp
+++ source/detail/math.hpp
@@ -1,4 +1,4 @@
-// Addition, subtraction, multiplication, division, unary minus
+// Overloads for the standard library math functions.
 // Copyright (C) 2013 David Stone
 //
 // This program is free software: you can redistribute it and / or modify
