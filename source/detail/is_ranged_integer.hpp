3d71bf41ed45bcde7a7ea909e4d85cb7f7011647
David Stone <david@doublewise.net>
2013-09-10 17:01:57 -0600

    Added some missing files.

---
+++ source/detail/is_ranged_integer.hpp
@@ -0,0 +1,44 @@
+// Determine if a class is a type of ranged_integer
+// Copyright (C) 2013 David Stone
+//
+// This program is free software: you can redistribute it and / or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef RANGED_INTEGER_IS_RANGED_INTEGER_HPP_
+#define RANGED_INTEGER_IS_RANGED_INTEGER_HPP_
+
+#include "forward_declaration.hpp"
+#include <cstdint>
+#include <type_traits>
+
+namespace detail {
+
+template<typename T>
+class is_ranged_integer_c {
+public:
+	static constexpr bool value = false;
+};
+template<intmax_t minimum, intmax_t maximum, template<intmax_t, intmax_t> class overflow_policy>
+class is_ranged_integer_c<ranged_integer<minimum, maximum, overflow_policy>> {
+public:
+	static constexpr bool value = true;
+};
+
+}	// namespace detail
+
+template<typename T>
+constexpr bool is_ranged_integer() noexcept {
+	return detail::is_ranged_integer_c<typename std::decay<T>::type>::value;
+}
+
+#endif	// RANGED_INTEGER_IS_RANGED_INTEGER_HPP_
