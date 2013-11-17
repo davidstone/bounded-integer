05a37d2f3073df6762bb0aac3b897176d83d460d
David Stone <david@doublewise.net>
2013-11-17 16:18:58 -0700

    Added volatile overloads to policy classes.

--- ranged_integer/detail/policy.hpp
+++ ranged_integer/detail/policy.hpp
@@ -26,8 +26,40 @@
 class null_policy {
 public:
 	constexpr null_policy() noexcept {}
+	constexpr null_policy(null_policy const &) noexcept {}
+	constexpr null_policy(null_policy &&) noexcept {}
+	constexpr null_policy(null_policy const volatile &) noexcept {}
+	constexpr null_policy(null_policy volatile &&) noexcept {}
+	null_policy & operator=(null_policy const &) noexcept {
+		return *this;
+	}
+	null_policy & operator=(null_policy &&) noexcept {
+		return *this;
+	}
+	null_policy & operator=(null_policy const volatile &) noexcept {
+		return *this;
+	}
+	null_policy & operator=(null_policy volatile &&) noexcept {
+		return *this;
+	}
+	null_policy volatile & operator=(null_policy const &) volatile noexcept {
+		return *this;
+	}
+	null_policy volatile & operator=(null_policy &&) volatile noexcept {
+		return *this;
+	}
+	null_policy volatile & operator=(null_policy const volatile &) volatile noexcept {
+		return *this;
+	}
+	null_policy volatile & operator=(null_policy volatile &&) volatile noexcept {
+		return *this;
+	}
 	template<typename integer>
 	constexpr integer assignment(intmax_t, intmax_t, integer const new_value) const noexcept {
+		return new_value;
+	}
+	template<typename integer>
+	constexpr integer assignment(intmax_t, intmax_t, integer const new_value) const volatile noexcept {
 		return new_value;
 	}
 
@@ -39,9 +71,45 @@ public:
 class throw_on_overflow {
 public:
 	constexpr throw_on_overflow() noexcept {}
+	constexpr throw_on_overflow(throw_on_overflow const &) noexcept {}
+	constexpr throw_on_overflow(throw_on_overflow &&) noexcept {}
+	constexpr throw_on_overflow(throw_on_overflow const volatile &) noexcept {}
+	constexpr throw_on_overflow(throw_on_overflow volatile &&) noexcept {}
+	throw_on_overflow & operator=(throw_on_overflow const &) noexcept {
+		return *this;
+	}
+	throw_on_overflow & operator=(throw_on_overflow &&) noexcept {
+		return *this;
+	}
+	throw_on_overflow & operator=(throw_on_overflow const volatile &) noexcept {
+		return *this;
+	}
+	throw_on_overflow & operator=(throw_on_overflow volatile &&) noexcept {
+		return *this;
+	}
+	throw_on_overflow volatile & operator=(throw_on_overflow const &) volatile noexcept {
+		return *this;
+	}
+	throw_on_overflow volatile & operator=(throw_on_overflow &&) volatile noexcept {
+		return *this;
+	}
+	throw_on_overflow volatile & operator=(throw_on_overflow const volatile &) volatile noexcept {
+		return *this;
+	}
+	throw_on_overflow volatile & operator=(throw_on_overflow volatile &&) volatile noexcept {
+		return *this;
+	}
 	// The optimizer should be able to simplify this to remove dead checks.
 	template<typename integer>
 	constexpr integer assignment(intmax_t minimum, intmax_t maximum, integer const new_value) const {
+		return (new_value < minimum) ?
+			throw std::underflow_error{"Value too small"} :
+			((new_value > maximum) ?
+				throw std::overflow_error{"Value too large"} :
+				new_value);
+	}
+	template<typename integer>
+	constexpr integer assignment(intmax_t minimum, intmax_t maximum, integer const new_value) const volatile {
 		return (new_value < minimum) ?
 			throw std::underflow_error{"Value too small"} :
 			((new_value > maximum) ?
@@ -56,8 +124,43 @@ public:
 class clamp_on_overflow {
 public:
 	constexpr clamp_on_overflow() noexcept {}
+	constexpr clamp_on_overflow(clamp_on_overflow const &) noexcept {}
+	constexpr clamp_on_overflow(clamp_on_overflow &&) noexcept {}
+	constexpr clamp_on_overflow(clamp_on_overflow const volatile &) noexcept {}
+	constexpr clamp_on_overflow(clamp_on_overflow volatile &&) noexcept {}
+	clamp_on_overflow & operator=(clamp_on_overflow const &) noexcept {
+		return *this;
+	}
+	clamp_on_overflow & operator=(clamp_on_overflow &&) noexcept {
+		return *this;
+	}
+	clamp_on_overflow & operator=(clamp_on_overflow const volatile &) noexcept {
+		return *this;
+	}
+	clamp_on_overflow & operator=(clamp_on_overflow volatile &&) noexcept {
+		return *this;
+	}
+	clamp_on_overflow volatile & operator=(clamp_on_overflow const &) volatile noexcept {
+		return *this;
+	}
+	clamp_on_overflow volatile & operator=(clamp_on_overflow &&) volatile noexcept {
+		return *this;
+	}
+	clamp_on_overflow volatile & operator=(clamp_on_overflow const volatile &) volatile noexcept {
+		return *this;
+	}
+	clamp_on_overflow volatile & operator=(clamp_on_overflow volatile &&) volatile noexcept {
+		return *this;
+	}
 	template<typename integer>
 	constexpr integer assignment(intmax_t minimum, intmax_t maximum, integer const new_value) const noexcept {
+		return
+			(new_value <= minimum) ? static_cast<integer>(minimum) :
+			(new_value >= maximum) ? static_cast<integer>(maximum) :
+			new_value;
+	}
+	template<typename integer>
+	constexpr integer assignment(intmax_t minimum, intmax_t maximum, integer const new_value) const volatile noexcept {
 		return
 			(new_value <= minimum) ? static_cast<integer>(minimum) :
 			(new_value >= maximum) ? static_cast<integer>(maximum) :
