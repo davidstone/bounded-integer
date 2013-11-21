51ddd0679d0257adefb8f2f265f5ffb6e99788eb
David Stone <david@doublewise.net>
2013-11-20 17:28:19 -0700

    Added a few technically unnecessary template instantiations to common_optional_type_t to minimize the risk of hitting the template instantiation limit when creating large arrays.

--- ranged_integer/detail/make_ranged_optional_array.hpp
+++ ranged_integer/detail/make_ranged_optional_array.hpp
@@ -32,6 +32,18 @@ class common_optional_type;
 template<typename... Ts>
 using common_optional_type_t = typename common_optional_type<typename std::decay<Ts>::type...>::type;
 
+// I manually stamp out a few of these templates to avoid hitting the template
+// instantiation limit of various compilers when creating large arrays.
+template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename... Ts>
+class common_optional_type<T1, T2, T3, T4, T5, T6, T7, T8, T9, Ts...> {
+public:
+	using type = common_optional_type_t<common_optional_type_t<T1, T2, T3, T4, T5, T6, T7, T8>, T9, Ts...>;
+};
+template<typename T1, typename T2, typename T3, typename T4, typename T5, typename... Ts>
+class common_optional_type<T1, T2, T3, T4, T5, Ts...> {
+public:
+	using type = common_optional_type_t<common_optional_type_t<T1, T2, T3, T4>, T5, Ts...>;
+};
 template<typename T1, typename T2, typename... Ts>
 class common_optional_type<T1, T2, Ts...> {
 public:
