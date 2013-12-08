2a7ebeb77e1877bb5cc7b3d515eed9185a8303d3
David Stone <david@doublewise.net>
2013-12-07 21:38:59 -0700

    Switched to C++1y mode.

--- build_scripts/gcc/std.py
+++ build_scripts/gcc/std.py
@@ -14,4 +14,4 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-cxx_std = ['-std=c++11']
+cxx_std = ['-std=c++1y']
