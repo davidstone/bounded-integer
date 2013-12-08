b6b3d427c178b9b6a54644535cd2b6f8b7248f9e
David Stone <david@doublewise.net>
2013-12-07 22:00:11 -0700

    Switched make_ranged_array to return my array type instead of std::array.

--- ranged_integer/detail/make_ranged_array.cpp
+++ ranged_integer/detail/make_ranged_array.cpp
@@ -24,24 +24,24 @@ namespace check_array {
 
 constexpr auto dynamic_int_array = make_ranged_array(0, 3, 6);
 static_assert(dynamic_int_array.size() == 3, "Array size wrong.");
-static_assert(dynamic_int_array[2] == 6, "Array element wrong.");
-static_assert(std::is_same<equivalent_type<int>, decay_t<decltype(dynamic_int_array[0])>>::value, "Array element type wrong for all int arguments.");
+static_assert(dynamic_int_array.at(2) == 6, "Array element wrong.");
+static_assert(std::is_same<equivalent_type<int>, decay_t<decltype(dynamic_int_array)::value_type>>::value, "Array element type wrong for all int arguments.");
 
 constexpr auto dynamic_mixed_array = make_ranged_array(-6, 15u);
 static_assert(dynamic_mixed_array.size() == 2, "Array size wrong.");
-static_assert(dynamic_mixed_array[0] == -6, "Array element wrong.");
+static_assert(dynamic_mixed_array.front() == -6, "Array element wrong.");
 static_assert(
 	std::is_same<
 		common_type_t<equivalent_type<int>, equivalent_type<unsigned>>,
-		decay_t<decltype(dynamic_mixed_array[0])>
+		decay_t<decltype(dynamic_mixed_array[0_ri])>
 	>::value,
 	"Array element type wrong for mixed int / unsigned arguments."
 );
 
 constexpr auto exact_array = make_ranged_array<-100, 5, 378, 23, 10000>();
 static_assert(exact_array.size() == 5, "Array size wrong.");
-static_assert(exact_array[2] == 378, "Array element wrong.");
-static_assert(std::is_same<ranged_integer<-100, 10000, null_policy>, decay_t<decltype(exact_array[0])>>::value, "Array element type wrong for exact template arguments.");
+static_assert(exact_array[2_ri] == 378, "Array element wrong.");
+static_assert(std::is_same<ranged_integer<-100, 10000, null_policy>, decay_t<decltype(exact_array[0_ri])>>::value, "Array element type wrong for exact template arguments.");
 
 }	// namespace check_array
 
@@ -56,8 +56,8 @@ constexpr auto value = multi_array<5, 4>::make_explicit(
 	0_ri, 0_ri, 0_ri, 0_ri
 );
 static_assert(value.size() == 5, "First dimension wrong.");
-static_assert(value[0].size() == 4, "Second dimension wrong.");
-static_assert(value[3][1] == 2467_ri, "Value wrong.");
+static_assert(value[0_ri].size() == 4, "Second dimension wrong.");
+static_assert(value[3_ri][1_ri] == 2467_ri, "Value wrong.");
 using value_type = decltype(value)::value_type::value_type;
 static_assert(std::numeric_limits<value_type>::min() == -4573, "min wrong");
 static_assert(std::numeric_limits<value_type>::max() == 2474, "max wrong");
@@ -83,9 +83,9 @@ constexpr auto four_dimensions = multi_array<second, third, fourth>::make_deduce
 	0_ri, 1_ri, 2_ri, 3_ri, 4_ri, 5_ri
 );
 static_assert(four_dimensions.size() == 1, "First dimension wrong.");
-static_assert(four_dimensions[0].size() == second, "Second dimension wrong.");
-static_assert(four_dimensions[0][0].size() == third, "Third dimension wrong.");
-static_assert(four_dimensions[0][0][0].size() == fourth, "Fourth dimension wrong.");
+static_assert(four_dimensions[0_ri].size() == second, "Second dimension wrong.");
+static_assert(four_dimensions[0_ri][0_ri].size() == third, "Third dimension wrong.");
+static_assert(four_dimensions[0_ri][0_ri][0_ri].size() == fourth, "Fourth dimension wrong.");
 
 }	// namespace check_multi_array
 
--- ranged_integer/detail/make_ranged_array.hpp
+++ ranged_integer/detail/make_ranged_array.hpp
@@ -1,4 +1,4 @@
-// Make a std::array of ranged_integer with automatically deduced size and type
+// Make an array of ranged_integer with automatically deduced size and type
 // Copyright (C) 2013 David Stone
 //
 // This program is free software: you can redistribute it and / or modify
@@ -17,116 +17,31 @@
 #ifndef RANGED_INTEGER_MAKE_RANGED_ARRAY_HPP_
 #define RANGED_INTEGER_MAKE_RANGED_ARRAY_HPP_
 
+#include "array.hpp"
 #include "common_type.hpp"
 #include "make_ranged.hpp"
-#include <array>
 #include <cstddef>
 #include <utility>
+#include <array>
 
 namespace detail {
 
-template<std::size_t... integers>
-class integer_sequence{};
-
-template<typename T1, typename T2>
-class concatenate;
-
-template<std::size_t... lhs, std::size_t... rhs>
-class concatenate<integer_sequence<lhs...>, integer_sequence<rhs...>> {
-public:
-	using type = integer_sequence<lhs..., rhs...>;
-};
-
-template<typename T1, typename T2>
-using concatenate_t = typename concatenate<T1, T2>::type;
-
-template<std::size_t min, std::size_t max>
-class make_integer_sequence_c;
-template<std::size_t min, std::size_t max>
-using make_integer_sequence = typename make_integer_sequence_c<min, max>::type;
-
-template<std::size_t min, std::size_t max>
-class make_integer_sequence_c {
-private:
-	// avoid overflow, although the compiler would probably run out of memory
-	// if it mattered
-	static constexpr std::size_t midpoint = min + (max - min) / 2;
-public:
-	static_assert(min < max, "Range inverted.");
-	using type = concatenate_t<make_integer_sequence<min, midpoint>, make_integer_sequence<midpoint + 1, max>>;
-};
-
-template<std::size_t value>
-class make_integer_sequence_c<value, value> {
-public:
-	using type = integer_sequence<value>;
-};
-
-
-// This gets the variadic parameter at position index
-template<std::size_t index>
-class variadic {
-public:
-	template<typename Integer, typename... Integers>
-	static constexpr auto get(Integer &&, Integers && ... integers) noexcept -> decltype(variadic<index - 1>::get(std::forward<Integers>(integers)...)) {
-		return variadic<index - 1>::get(std::forward<Integers>(integers)...);
-	}
-};
-template<>
-class variadic<0> {
-public:
-	template<typename Integer, typename... Integers>
-	static constexpr Integer && get(Integer && integer, Integers && ...) noexcept {
-		return std::forward<Integer>(integer);
-	}
-};
-
-// No deduced size
 template<typename T, std::size_t... dimensions>
-class multi_array;
+class array_c;
 
-// A lot of this is greatly simplified in C++14 with relaxed rules for braces
+template<typename T, std::size_t... dimensions>
+using array_type = typename array_c<T, dimensions...>::type;
+
 template<typename T, std::size_t dimension, std::size_t... dimensions>
-class multi_array<T, dimension, dimensions...> {
-private:
+class array_c<T, dimension, dimensions...> {
 public:
-	template<std::size_t... indexes, typename... Integers>
-	static constexpr typename multi_array<T, dimensions...>::type apply_n(integer_sequence<indexes...>, Integers && ... integers) noexcept {
-		return multi_array<T, dimensions...>::make(variadic<indexes>::get(std::forward<Integers>(integers)...)...);
-	}
-
-	using type = std::array<typename multi_array<T, dimensions...>::type, dimension>;
-
-	
-	// Break the variadic pack into `dimension` chunks. Construct `dimension`
-	// nested types with each chunk.
-	
-	template<std::size_t... indexes, typename... Integers>
-	static constexpr type make_helper(integer_sequence<indexes...>, Integers && ... integers) noexcept {
-		return type{{
-			apply_n(
-				make_integer_sequence<
-					indexes * (sizeof...(Integers) / dimension),
-					(indexes + 1) * (sizeof...(Integers) / dimension) - 1
-				>{},
-				std::forward<Integers>(integers)...
-			// This final ... should expand `indexes`
-			)...
-		}};
-	}
-	template<typename... Integers>
-	static constexpr type make(Integers && ... integers) noexcept {
-		return make_helper(make_integer_sequence<0, dimension - 1>{}, std::forward<Integers>(integers)...);
-	}
+	using type = array<array_type<T, dimensions...>, dimension>;
 };
 
 template<typename T>
-class multi_array<T> {
+class array_c<T> {
 public:
 	using type = T;
-	static constexpr type make(T && integer) noexcept {
-		return static_cast<type>(std::forward<T>(integer));
-	}
 };
 
 template<std::size_t dimension, std::size_t... dimensions>
@@ -155,16 +70,16 @@ template<std::size_t... dimensions>
 class multi_array {
 public:
 	// make_explicit assumes that all of the dimensions have been passed in.
-	// make_deduced assumes you did not specify the first dimension.
 	template<
 		typename... Integers,
 		typename common_t = common_type_t<equivalent_type<Integers>...>,
-		typename result_type = detail::multi_array<common_t, dimensions...>
+		typename result_type = detail::array_type<common_t, dimensions...>
 	>
-	static constexpr typename result_type::type make_explicit(Integers && ... integers) noexcept {
-		return result_type::make(std::forward<Integers>(integers)...);
+	static constexpr result_type make_explicit(Integers && ... integers) noexcept {
+		return result_type{ static_cast<common_t>(std::forward<Integers>(integers))... };
 	}
 
+	// make_deduced assumes you did not specify the first dimension.
 	template<typename... Integers>
 	static constexpr auto make_deduced(Integers && ... integers) noexcept -> decltype(multi_array<detail::final_dimension<sizeof...(Integers), dimensions...>::value, dimensions...>::make_explicit(std::forward<Integers>(integers)...)) {
 		return multi_array<detail::final_dimension<sizeof...(Integers), dimensions...>::value, dimensions...>::make_explicit(std::forward<Integers>(integers)...);
