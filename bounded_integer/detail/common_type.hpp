94fa88a8f7215fa2bb73a0634e48375319317342
David Stone <david@doublewise.net>
2013-12-13 07:28:22 -0700

    Expanded common_type specializations to reduce template instantiation depth.

--- bounded_integer/detail/common_type.hpp
+++ bounded_integer/detail/common_type.hpp
@@ -107,6 +107,14 @@ public:
 	using type = bounded_integer::common_type_t<bounded_integer::common_type_t<type0, T1, T2, T3, T4, T5, T6, T7, T8>, T9, Ts...>;
 };
 
+template<intmax_t minimum, intmax_t maximum, typename overflow_policy, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename... Ts>
+struct common_type<bounded_integer::bounded_integer<minimum, maximum, overflow_policy>, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, Ts...> {
+private:
+	using type0 = bounded_integer::bounded_integer<minimum, maximum, overflow_policy>;
+public:
+	using type = bounded_integer::common_type_t<bounded_integer::common_type_t<type0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>, T17, Ts...>;
+};
+
 }	// namespace std
 
 #endif	// BOUNDED_INTEGER_COMMON_TYPE_HPP_
