75f7ca718891d459bd88856945b207e155ec073f
David Stone <david@doublewise.net>
2014-07-26 15:16:49 -0600

    Debug symbols are not yet supported in clang for automatic return type deduction.

--- build_scripts/clang/debug.py
+++ build_scripts/clang/debug.py
@@ -1,5 +1,5 @@
 # Include debug symbols
-# Copyright (C) 2012 David Stone
+# Copyright (C) 2014 David Stone
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,4 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-debug = ['-g']
+debug = [
+#	'-g'
+]
