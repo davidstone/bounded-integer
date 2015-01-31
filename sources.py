7a163042e0552c221b7f47e92b724ca31b74a700
David Stone <david@doublewise.net>
2015-01-31 11:05:11 -0700

    Added scons-template hg subrepo.

--- sources.py
+++ sources.py
@@ -1,5 +1,5 @@
 # List of sources
-# Copyright (C) 2014 David Stone
+# Copyright (C) 2015 David Stone
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,73 +14,75 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-def prepend_dir(directory, sources):
-	"""Remove redundant specification of a directory for multiple sources"""
-	return map(lambda source: directory + '/' + source, sources)
+from program import prepend_dir, Program
 
-# Example of use:
-#
-# example_sources = prepend_dir('cool', ['alright.cpp', 'awesome.cpp'])
-# example_sources += prepend_dir('yeah', ['no.cpp', 'maybe.cpp'])
-# example_libraries = ['boost_filesystem']
-#
-# example = ('example_program_name', example_sources, example_libraries)
-# base_sources = [example]
+source_directory = 'bounded_integer'
 
-base_sources = [('test',
-	prepend_dir('detail', [
-		'algorithm.cpp',
-		'cast.cpp',
-		'class.cpp',
-		'common_type.cpp',
-		'common_type_and_value_category.cpp',
-		'conditional.cpp',
-		'enable_if.cpp',
-		'forward_declaration.cpp',
-		'integer_range.cpp',
-		'is_bounded_integer.cpp',
-		'iterator.cpp',
-		'literal.cpp',
-		'make.cpp',
-		'math.cpp',
-		'minmax.cpp',
-		'noexcept.cpp',
-		'numeric_limits.cpp',
-		'overlapping_range.cpp',
-		'stream.cpp',
-		'string.cpp',
-		'typedefs.cpp',
-		'underlying_type.cpp',
-	]) +
-	prepend_dir('detail/array', [
-		'array.cpp',
-		'make_array.cpp',
-	]) +
-	prepend_dir('detail/operators', [
-		'arithmetic.cpp',
-		'arithmetic_builtin.cpp',
-		'arithmetic_result_type.cpp',
-		'comparison.cpp',
-		'comparison_builtin.cpp',
-		'compound_assignment.cpp',
-		'operators.cpp',
-	]) +
-	prepend_dir('detail/optional', [
-		'forward_declaration.cpp',
-		'make_optional_array.cpp',
-		'optional.cpp',
-		'comparison_operators.cpp',
-	]) +
-	prepend_dir('detail/policy', [
-		'all.cpp',
-		'basic_policy.cpp',
-		'clamp_policy.cpp',
-		'common_policy.cpp',
-		'dynamic_policy.cpp',
-		'is_overflow_policy.cpp',
-		'modulo_policy.cpp',
-		'null_policy.cpp',
-		'throw_policy.cpp',
-	]) +
-	['array.cpp', 'integer_range.cpp', 'optional.cpp', 'bounded_integer.cpp']
-, [])]
+sources = [
+	'array.cpp',
+	'integer_range.cpp',
+	'optional.cpp',
+	'bounded_integer.cpp'
+]
+sources += prepend_dir('detail', [
+	'algorithm.cpp',
+	'cast.cpp',
+	'class.cpp',
+	'common_type.cpp',
+	'common_type_and_value_category.cpp',
+	'conditional.cpp',
+	'enable_if.cpp',
+	'forward_declaration.cpp',
+	'integer_range.cpp',
+	'is_bounded_integer.cpp',
+	'iterator.cpp',
+	'literal.cpp',
+	'make.cpp',
+	'math.cpp',
+	'minmax.cpp',
+	'noexcept.cpp',
+	'numeric_limits.cpp',
+	'overlapping_range.cpp',
+	'stream.cpp',
+	'string.cpp',
+	'typedefs.cpp',
+	'underlying_type.cpp',
+])
+sources += prepend_dir('detail/array', [
+	'array.cpp',
+	'make_array.cpp',
+])
+sources += prepend_dir('detail/operators', [
+	'arithmetic.cpp',
+	'arithmetic_builtin.cpp',
+	'arithmetic_result_type.cpp',
+	'comparison.cpp',
+	'comparison_builtin.cpp',
+	'compound_assignment.cpp',
+	'operators.cpp',
+])
+sources += prepend_dir('detail/optional', [
+	'forward_declaration.cpp',
+	'make_optional_array.cpp',
+	'optional.cpp',
+	'comparison_operators.cpp',
+])
+sources += prepend_dir('detail/policy', [
+	'all.cpp',
+	'basic_policy.cpp',
+	'clamp_policy.cpp',
+	'common_policy.cpp',
+	'dynamic_policy.cpp',
+	'is_overflow_policy.cpp',
+	'modulo_policy.cpp',
+	'null_policy.cpp',
+	'throw_policy.cpp',
+])
+
+
+programs = [
+	Program(
+		'test',
+		sources = sources
+	)
+]
