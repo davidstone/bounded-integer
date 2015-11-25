8dec04d6be712b786d80a77c97fbfbeeebbbb082
David Stone <david@doublewise.net>
2015-11-24 21:05:28 -0700

    Improved definition of index_type to not cause a circular definition.

--- include/containers/index_type.hpp
+++ include/containers/index_type.hpp
@@ -16,9 +16,6 @@
 
 #pragma once
 
-#include <containers/is_container.hpp>
-#include <containers/is_iterator.hpp>
-
 #include <bounded_integer/bounded_integer.hpp>
 
 #include <limits>
@@ -27,29 +24,21 @@
 namespace containers {
 namespace detail {
 
-template<typename T, bool container = is_container<T>>
-struct index_type_source_c;
-
-template<typename Container>
-struct index_type_source_c<Container, true> {
-	using type = typename Container::size_type;
-};
-
-template<typename Iterator>
-struct index_type_source_c<Iterator, false> {
-	static_assert(is_iterator<Iterator>);
-	using type = typename Iterator::difference_type;
-};
-
 template<typename T>
-using index_type_source = typename index_type_source_c<std::decay_t<T>>::type;
+constexpr auto index_type_source() -> typename T::size_type {
+	return std::numeric_limits<typename T::size_type>::min();
+}
+template<typename T>
+constexpr auto index_type_source() -> typename T::difference_type {
+	return std::numeric_limits<typename T::difference_type>::min();
+}
 
 }	// namespace detail
 
 template<typename T>
 using index_type = bounded::checked_integer<
 	0,
-	static_cast<std::intmax_t>(std::numeric_limits<detail::index_type_source<T>>::max()) - 1,
+	static_cast<std::intmax_t>(std::numeric_limits<decltype(detail::index_type_source<std::decay_t<T>>())>::max()) - 1,
 	std::out_of_range
 >;
 
