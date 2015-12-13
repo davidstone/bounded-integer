6f5d32b2ca27745fb3b3ef3377893674a0522ee7
David Stone <david@doublewise.net>
2015-12-12 17:42:50 -0700

    Changed license from AGPLv3+ to Boost Software License.

--- bounded_integer/detail/overlapping_range.cpp
+++ bounded_integer/detail/overlapping_range.cpp
@@ -1,18 +1,7 @@
-// Verify that the header can stand on its own
-// Copyright (C) 2014 David Stone
-//
-// This program is free software: you can redistribute it and / or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// Copyright David Stone 2015.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #include "overlapping_range.hpp"
 
--- bounded_integer/detail/requires.hpp
+++ bounded_integer/detail/requires.hpp
@@ -1,18 +1,7 @@
-// Improved version of enable_if
-// Copyright (C) 2015 David Stone
-//
-// This program is free software: you can redistribute it and / or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// Copyright David Stone 2015.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
 
