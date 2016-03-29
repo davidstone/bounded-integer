4caabc4d538cab1f070f9fc12da8c41d49b386c9
David Stone <david@doublewise.net>
2016-03-28 20:22:23 -0600

    Added all_equal, any_equal, and none_equal. any_equal is a replacement for find(first, last, variable) != last

--- include/containers/algorithms/all_any_none.hpp
+++ include/containers/algorithms/all_any_none.hpp
@@ -17,15 +17,27 @@ template<typename InputIterator, typename Sentinel, typename UnaryPredicate>
 constexpr auto all_of(InputIterator const first, Sentinel const last, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
 	::containers::find_if_not(first, last, p) == last
 )
+template<typename InputIterator, typename Sentinel, typename T>
+constexpr auto all_equal(InputIterator const first, Sentinel const last, T && value) BOUNDED_NOEXCEPT_VALUE(
+	all_of(first, last, bounded::equal_to(std::forward<T>(value)))
+)
 
 template<typename InputIterator, typename Sentinel, typename UnaryPredicate>
 constexpr auto any_of(InputIterator const first, Sentinel const last, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
 	::containers::find_if(first, last, p) != last
 )
+template<typename InputIterator, typename Sentinel, typename T>
+constexpr auto any_equal(InputIterator const first, Sentinel const last, T && value) BOUNDED_NOEXCEPT_VALUE(
+	any_of(first, last, bounded::equal_to(std::forward<T>(value)))
+)
 
 template<typename InputIterator, typename Sentinel, typename UnaryPredicate>
 constexpr auto none_of(InputIterator const first, Sentinel const last, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
 	::containers::find_if(first, last, p) == last
+)
+template<typename InputIterator, typename Sentinel, typename T>
+constexpr auto none_equal(InputIterator const first, Sentinel const last, T && value) BOUNDED_NOEXCEPT_VALUE(
+	none_of(first, last, bounded::equal_to(std::forward<T>(value)))
 )
 
 }	// namespace containers
