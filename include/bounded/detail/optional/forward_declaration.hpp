4385da34b778313277c6917e85ceb596fb86e6ee
David Stone <david@doublewise.net>
2016-04-17 14:45:33 -0600

    none_t should not be implicitly constructible from int.

--- include/bounded/detail/optional/forward_declaration.hpp
+++ include/bounded/detail/optional/forward_declaration.hpp
@@ -1,4 +1,4 @@
-// Copyright David Stone 2015.
+// Copyright David Stone 2016.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -22,7 +22,7 @@ private:
 
 // none_t cannot be default constructible or we get an ambiguity in op = {};
 struct none_t {
-	constexpr none_t(int) noexcept {}
+	constexpr explicit none_t(int) noexcept {}
 };
 constexpr auto none = none_t(0);
 
