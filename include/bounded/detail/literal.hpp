625a52945109e05d067e1f676e925ba0bb582199
David Stone <david@doublewise.net>
2016-11-25 12:09:34 -0700

    Use inline variables instead of a function.

--- include/bounded/detail/literal.hpp
+++ include/bounded/detail/literal.hpp
@@ -12,30 +12,22 @@
 namespace bounded {
 namespace detail {
 
-constexpr inline auto power(intmax_t const radix, intmax_t const exponent) noexcept -> intmax_t {
+constexpr auto power(intmax_t const radix, intmax_t const exponent) noexcept -> intmax_t {
 	return (exponent == 0) ? 1 : radix * power(radix, exponent - 1);
 }
 
-// I have to define value as a static member function due to a clang linker
-// error. It seems that it should be usable as a static member variable instead,
-// and that would simplify things a bit. It appears to be a bug in clang because
-// it triggers when unrelated parts of my code change, but I am not sure.
 template<char digit, char... digits>
 struct literal {
 private:
 	static constexpr intmax_t radix = 10;
 	static constexpr intmax_t integer_scale = power(radix, sizeof...(digits));
 public:
-	static constexpr auto value() noexcept {
-		return literal<digit>::value() * constant<integer_scale> + literal<digits...>::value();
-	}
+	static constexpr auto value = literal<digit>::value * constant<integer_scale> + literal<digits...>::value;
 };
 
 template<char digit>
 struct literal<digit> {
-	static constexpr auto value() noexcept {
-		return constant<digit - '0'>;
-	}
+	static constexpr auto value = constant<digit - '0'>;
 };
 
 }	// namespace detail
@@ -44,7 +36,7 @@ namespace literal {
 
 template<char... digits>
 constexpr auto operator"" _bi() noexcept {
-	return detail::literal<digits...>::value();
+	return detail::literal<digits...>::value;
 }
 
 }	// namespace literal
