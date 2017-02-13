540c10a966e24a1fd70dcb9fb570154be2baff67
David Stone <david@doublewise.net>
2017-02-13 14:17:12 -0700

    Use if constexpr to improve overlapping_range functions.

--- include/bounded/detail/overlapping_range.hpp
+++ include/bounded/detail/overlapping_range.hpp
@@ -20,32 +20,34 @@ namespace detail {
 template<typename T>
 constexpr auto value_fits_in_type(intmax_t const value) noexcept {
 	static_assert(basic_numeric_limits<T>::is_specialized, "Only works with integer types.");
-	if (std::is_same<T, uintmax_t>::value) {
+	if constexpr (std::is_same<T, uintmax_t>::value) {
 		return value >= 0;
+	} else {
+		using limits = basic_numeric_limits<T>;
+		return limits::min() <= value and value <= limits::max();
 	}
-	return basic_numeric_limits<T>::min() <= value and value <= basic_numeric_limits<T>::max();
 }
 
 template<typename T>
 constexpr auto type_overlaps_range(intmax_t const minimum, intmax_t const maximum) noexcept {
 	static_assert(basic_numeric_limits<T>::is_specialized, "Only works with integer types.");
-	if (std::is_same<T, uintmax_t>::value) {
+	if constexpr (std::is_same<T, uintmax_t>::value) {
 		return maximum >= 0;
+	} else {
+		using limits = basic_numeric_limits<T>;
+		return minimum <= limits::max() and limits::min() <= maximum;
 	}
-	return
-		minimum <= basic_numeric_limits<T>::max() and
-		basic_numeric_limits<T>::min() <= maximum;
 }
 
 template<typename T>
 constexpr auto type_fits_in_range(intmax_t const minimum, intmax_t const maximum) noexcept {
 	static_assert(basic_numeric_limits<T>::is_specialized, "Only works with integer types.");
-	if (std::is_same<T, uintmax_t>::value) {
-		return false;
+	if constexpr (std::is_same<T, uintmax_t>::value) {
+		return std::false_type{};
+	} else {
+		using limits = basic_numeric_limits<T>;
+		return minimum <= limits::min() and limits::max() <= maximum;
 	}
-	return
-		minimum <= basic_numeric_limits<T>::min() and
-		basic_numeric_limits<T>::max() <= maximum;
 }
 
 
@@ -58,10 +60,8 @@ constexpr auto types_overlap() {
 	} else if constexpr (basic_numeric_limits<LHS>::is_specialized and basic_numeric_limits<RHS>::is_specialized) {
 		using type = std::decay_t<std::conditional_t<lhs_uintmax, LHS, RHS>>;
 		using range_type = std::decay_t<std::conditional_t<lhs_uintmax, RHS, LHS>>;
-		return type_overlaps_range<type>(
-			basic_numeric_limits<range_type>::min(),
-			basic_numeric_limits<range_type>::max()
-		);
+		using limits = basic_numeric_limits<range_type>;
+		return type_overlaps_range<type>(limits::min(), limits::max());
 	} else {
 		return std::false_type{};
 	}
