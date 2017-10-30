a2bd4f57b5695627c09850a7064fad94814de774
David Stone <david@doublewise.net>
2017-10-31 00:06:17 +0100

    Removed spurious comment.

--- include/bounded/detail/comparison.hpp
+++ include/bounded/detail/comparison.hpp
@@ -1,4 +1,4 @@
-// Copyright David Stone 2016.
+// Copyright David Stone 2017.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -15,10 +15,6 @@
 #include <type_traits>
 
 namespace bounded {
-
-// Unfortunately, (bool ? derived const & : base) is not allowed due to the
-// const qualification. Fortunately, we can fix this by returning a
-// const-qualified prvalue from all compare functions.
 
 struct strong_ordering {
 	constexpr explicit strong_ordering(int value_) noexcept:
