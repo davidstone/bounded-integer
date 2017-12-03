1e6241fcd0ef1347cf7ddaf83f14317010d859c7
David Stone <david@doublewise.net>
2017-12-03 13:11:34 -0500

    Removed unused function.

--- include/bounded/detail/overlapping_range.hpp
+++ include/bounded/detail/overlapping_range.hpp
@@ -51,22 +51,5 @@ constexpr auto type_fits_in_range(intmax_t const minimum, intmax_t const maximum
 	}
 }
 
-
-template<typename LHS, typename RHS>
-constexpr auto types_overlap() {
-	constexpr auto lhs_uintmax = std::is_same<LHS, std::uintmax_t>{};
-	constexpr auto rhs_uintmax = std::is_same<RHS, std::uintmax_t>{};
-	if constexpr (lhs_uintmax and rhs_uintmax) {
-		return std::true_type{};
-	} else if constexpr (basic_numeric_limits<LHS>::is_specialized and basic_numeric_limits<RHS>::is_specialized) {
-		using type = std::decay_t<std::conditional_t<lhs_uintmax, LHS, RHS>>;
-		using range_type = std::decay_t<std::conditional_t<lhs_uintmax, RHS, LHS>>;
-		using limits = basic_numeric_limits<range_type>;
-		return type_overlaps_range<type>(limits::min(), limits::max());
-	} else {
-		return std::false_type{};
-	}
-}
-
 }	// namespace detail
 }	// namespace bounded
