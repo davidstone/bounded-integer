dbafcbcbfafa0905a1446cb4fe688e19dc0925de
David Stone <david@doublewise.net>
2017-12-03 20:18:25 -0500

    Implement overlapping range functions in terms of compare instead fo the comparison operators. This allows the implementation to work on all integer types generically, rather than having a special case for uintmax_t.

--- include/bounded/detail/overlapping_range.hpp
+++ include/bounded/detail/overlapping_range.hpp
@@ -1,4 +1,4 @@
-// Copyright David Stone 2015.
+// Copyright David Stone 2017.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -7,49 +7,31 @@
 
 #include <bounded/detail/basic_numeric_limits.hpp>
 #include <bounded/detail/comparison.hpp>
-#include <bounded/detail/forward_declaration.hpp>
-
-#include <cstdint>
-#include <type_traits>
 
 // All of these functions have a precondition that minimum <= maximum
 
 namespace bounded {
 namespace detail {
 
-template<typename T>
-constexpr auto value_fits_in_type(intmax_t const value) noexcept {
+template<typename T, typename Value>
+constexpr auto value_fits_in_type(Value const value) noexcept {
 	static_assert(basic_numeric_limits<T>::is_specialized, "Only works with integer types.");
-	if constexpr (std::is_same<T, uintmax_t>{}) {
-		return value >= 0;
-	} else {
-		using limits = basic_numeric_limits<T>;
-		return limits::min() <= value and value <= limits::max();
-	}
+	using limits = basic_numeric_limits<T>;
+	return compare(limits::min(), value) <= 0 and compare(value, limits::max()) <= 0;
 }
 
-template<typename T>
-constexpr auto type_overlaps_range(intmax_t const minimum, intmax_t const maximum) noexcept {
+template<typename T, typename Minimum, typename Maximum>
+constexpr auto type_overlaps_range(Minimum const minimum, Maximum const maximum) noexcept {
 	static_assert(basic_numeric_limits<T>::is_specialized, "Only works with integer types.");
-	if constexpr (std::is_same<T, uintmax_t>{}) {
-		static_cast<void>(minimum);
-		return maximum >= 0;
-	} else {
-		using limits = basic_numeric_limits<T>;
-		return minimum <= limits::max() and limits::min() <= maximum;
-	}
+	using limits = basic_numeric_limits<T>;
+	return compare(limits::min(), maximum) <= 0 and compare(minimum, limits::max()) <= 0;
 }
 
-template<typename T>
-constexpr auto type_fits_in_range(intmax_t const minimum, intmax_t const maximum) noexcept {
+template<typename T, typename Minimum, typename Maximum>
+constexpr auto type_fits_in_range(Minimum const minimum, Maximum const maximum) noexcept {
 	static_assert(basic_numeric_limits<T>::is_specialized, "Only works with integer types.");
-	if constexpr (std::is_same<T, uintmax_t>{}) {
-		return std::false_type{};
-	} else {
-		using limits = basic_numeric_limits<T>;
-		return minimum <= limits::min() and limits::max() <= maximum;
-	}
+	using limits = basic_numeric_limits<T>;
+	return compare(minimum, limits::min()) <= 0 and compare(limits::max(), maximum) <= 0;
 }
-
 }	// namespace detail
 }	// namespace bounded
