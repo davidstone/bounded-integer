ab8e700cc16928b8e1a41b0254e6a6f5349ecc3b
David Stone <david@doublewise.net>
2017-12-07 16:42:33 -0500

    Added more unary minus tests, to check std::numeric_limits<T>::min and ::max for 8, 16, 32, and 64 bit integers.

--- source/bounded/detail/arithmetic/operators.cpp
+++ source/bounded/detail/arithmetic/operators.cpp
@@ -48,13 +48,40 @@ static_assert(homogeneous_equals(
 	bounded::constant<0>
 ));
 static_assert(homogeneous_equals(
-	-signed_max,
-	bounded::constant<-static_cast<std::intmax_t>(signed_max)>
+	-bounded::constant<1>,
+	bounded::constant<-1>
 ));
-static_assert(homogeneous_equals(
-	-signed_min,
-	bounded::constant<-static_cast<std::uintmax_t>(signed_min)>
-));
+
+template<typename Signed, typename Unsigned>
+constexpr auto check_signed_limits() {
+	// TODO: Make this test work when min() == -max()
+	static_assert(homogeneous_equals(
+		-bounded::constant<std::numeric_limits<Signed>::max()>,
+		bounded::constant<-std::numeric_limits<Signed>::max()>
+	));
+	static_assert(homogeneous_equals(
+		-bounded::constant<std::numeric_limits<Signed>::min()>,
+		// The outer cast to unsigned is to counteract integer promotion
+		bounded::constant<static_cast<Unsigned>(-static_cast<Unsigned>(std::numeric_limits<Signed>::min()))>
+	));
+}
+template<typename Signed, typename Unsigned, typename LargerSigned>
+constexpr auto check_limits() {
+	check_signed_limits<Signed, Unsigned>();
+	static_assert(homogeneous_equals(
+		-bounded::constant<std::numeric_limits<Unsigned>::max()>,
+		bounded::constant<-static_cast<LargerSigned>(std::numeric_limits<Unsigned>::max())>
+	));
+}
+
+constexpr auto check_all_limits() {
+	check_limits<std::int8_t, std::uint8_t, std::int16_t>();
+	check_limits<std::int16_t, std::uint16_t, std::int32_t>();
+	check_limits<std::int32_t, std::uint32_t, std::int64_t>();
+	check_signed_limits<std::int64_t, std::uint64_t>();
+}
+static_assert((static_cast<void>(check_all_limits()), true));
+
 static_assert(homogeneous_equals(
 	-x,
 	bounded::checked_integer<-10, -1>(-9, bounded::non_check)
