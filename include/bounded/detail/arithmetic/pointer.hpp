044be8a5252a76df40a286331186a5bc19947723
David Stone <david@doublewise.net>
2017-12-07 17:36:37 -0500

    Compare against a bounded::constant instead of an integer literal.

--- include/bounded/detail/arithmetic/pointer.hpp
+++ include/bounded/detail/arithmetic/pointer.hpp
@@ -1,4 +1,4 @@
-// Copyright David Stone 2015.
+// Copyright David Stone 2017.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -6,7 +6,7 @@
 #pragma once
 
 #include <bounded/detail/arithmetic/common.hpp>
-
+#include <bounded/detail/class.hpp>
 #include <bounded/detail/is_bounded_integer.hpp>
 #include <bounded/detail/requires.hpp>
 
@@ -14,7 +14,7 @@ namespace bounded {
 
 template<typename T, typename Integer, BOUNDED_REQUIRES(
 	is_bounded_integer<Integer> and
-	(std::is_pointer<T>{} or (std::is_array<T>{} and Integer::max() <= std::extent<T>{}))
+	(std::is_pointer<T>{} or (std::is_array<T>{} and Integer::max() <= constant<std::extent<T>::value>))
 )>
 constexpr auto operator+(T const & array, Integer const number) noexcept {
 	return array + number.value();
@@ -23,7 +23,7 @@ constexpr auto operator+(T const & array, Integer const number) noexcept {
 
 template<typename Integer, typename T, BOUNDED_REQUIRES(
 	is_bounded_integer<Integer> and
-	(std::is_pointer<T>{} or (std::is_array<T>{} and Integer::max() <= std::extent<T>{}))
+	(std::is_pointer<T>{} or (std::is_array<T>{} and Integer::max() <= constant<std::extent<T>::value>))
 )>
 constexpr auto operator+(Integer const number, T const & array) noexcept {
 	return number.value() + array;
