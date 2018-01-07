c5b3628f7f0aa5ebc4986fc35805202338d28201
David Stone <david@doublewise.net>
2018-01-07 12:15:37 -0500

    Implement array with multidimensional support directly, rather than going through some typedefs.

--- include/containers/array/array.hpp
+++ include/containers/array/array.hpp
@@ -1,4 +1,4 @@
-// Copyright David Stone 2016.
+// Copyright David Stone 2018.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -19,36 +19,55 @@
 #include <utility>
 
 namespace containers {
+
+template<typename T, std::size_t size, std::size_t... sizes>
+struct array;
+
 namespace detail {
 
-template<typename T, std::size_t size>
+template<typename T, std::size_t... sizes>
+struct array_value_type {
+	using type = array<T, sizes...>;
+};
+template<typename T>
+struct array_value_type<T> {
+	using type = T;
+};
+
+}	// namespace detail
+
+template<typename T, std::size_t size, std::size_t... sizes>
 struct array {
-	using value_type = T;
+	using value_type = typename detail::array_value_type<T, sizes...>::type;
 
 	using size_type = bounded::constant_t<size>;
 	
-	using const_iterator = basic_array_iterator<value_type const, array>;
-	using iterator = basic_array_iterator<value_type, array>;
+	using const_iterator = detail::basic_array_iterator<value_type const, array>;
+	using iterator = detail::basic_array_iterator<value_type, array>;
 
-	CONTAINERS_OPERATOR_BRACKET_DEFINITIONS
+	CONTAINERS_OPERATOR_BRACKET_DEFINITIONS(array)
 
 	// Consider this private. It must be public for the class to be an
 	// aggregate
 	value_type m_value[size];
 };
 
+namespace detail {
+
 struct aggregate_initialization {
 };
 
-template<typename T>
-struct array<T, 0> {
-	using value_type = T;
+}	// namespace detail
+
+template<typename T, std::size_t... sizes>
+struct array<T, 0, sizes...> {
+	using value_type = typename detail::array_value_type<T, sizes...>::type;
 
 	using size_type = bounded::constant_t<0>;
 	
-	using const_iterator = basic_array_iterator<value_type const, array>;
+	using const_iterator = detail::basic_array_iterator<value_type const, array>;
 	// TODO: Should this be using iterator = const_iterator?
-	using iterator = basic_array_iterator<value_type, array>;
+	using iterator = detail::basic_array_iterator<value_type, array>;
 	
 	constexpr array() noexcept = default;
 
@@ -57,55 +76,43 @@ struct array<T, 0> {
 	//
 	// This extra parameter is an aggregate and thus can be constructed with {}.
 	// This constructor allows the syntax array<T, 0> = {{}}
-	constexpr array(aggregate_initialization) noexcept {}
+	constexpr array(detail::aggregate_initialization) noexcept {}
 
 	// No operator[]
 };
 
-template<typename T, std::size_t size>
-constexpr auto begin(array<T, size> const & container) noexcept {
-	return typename array<T, size>::const_iterator(container.m_value, detail::iterator_constructor);
+template<typename... Args>
+array(Args && ...) -> array<std::common_type_t<std::decay_t<Args>...>, sizeof...(Args)>;
+
+template<typename T, std::size_t... sizes>
+constexpr auto begin(array<T, sizes...> const & container) noexcept {
+	return typename array<T, sizes...>::const_iterator(container.m_value, detail::iterator_constructor);
 }
-template<typename T, std::size_t size>
-constexpr auto begin(array<T, size> & container) noexcept {
-	return typename array<T, size>::iterator(container.m_value, detail::iterator_constructor);
+template<typename T, std::size_t... sizes>
+constexpr auto begin(array<T, sizes...> & container) noexcept {
+	return typename array<T, sizes...>::iterator(container.m_value, detail::iterator_constructor);
 }
 
-template<typename T>
-constexpr auto begin(array<T, 0> const &) noexcept {
-	return typename array<T, 0>::const_iterator();
+template<typename T, std::size_t... sizes>
+constexpr auto begin(array<T, 0, sizes...> const &) noexcept {
+	return typename array<T, 0, sizes...>::const_iterator();
 }
-template<typename T>
-constexpr auto begin(array<T, 0> &) noexcept {
-	return typename array<T, 0>::iterator();
+template<typename T, std::size_t... sizes>
+constexpr auto begin(array<T, 0, sizes...> &) noexcept {
+	return typename array<T, 0, sizes...>::iterator();
 }
 
-template<typename T, std::size_t size>
-constexpr auto end(array<T, size> const & container) noexcept {
+template<typename T, std::size_t size, std::size_t... sizes>
+constexpr auto end(array<T, size, sizes...> const & container) noexcept {
 	return begin(container) + bounded::constant<size>;
 }
-template<typename T, std::size_t size>
-constexpr auto end(array<T, size> & container) noexcept {
+template<typename T, std::size_t size, std::size_t... sizes>
+constexpr auto end(array<T, size, sizes...> & container) noexcept {
 	return begin(container) + bounded::constant<size>;
 }
 
 
-template<typename T, std::size_t dimension, std::size_t... dimensions>
-struct multi_dimensional_array {
-	using type = array<typename multi_dimensional_array<T, dimensions...>::type, dimension>;
-};
-
-template<typename T, std::size_t dimension>
-struct multi_dimensional_array<T, dimension> {
-	using type = array<T, dimension>;
-};
-
-}	// namespace detail
-
-template<typename T, std::size_t... dimensions>
-using array = typename detail::multi_dimensional_array<T, dimensions...>::type;
-
-template<typename T, std::size_t size>
-struct is_container_c<detail::array<T, size>> : std::true_type {};
+template<typename T, std::size_t... sizes>
+struct is_container_c<array<T, sizes...>> : std::true_type {};
 
 }	// namespace containers
