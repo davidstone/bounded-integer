e4f3e014d8b4fd56049f93a019df8a48a9306793
David Stone <david@doublewise.net>
2018-05-28 23:54:22 +0100

    Moved strong_ordering into its own file.

--- include/bounded/detail/comparison.hpp
+++ include/bounded/detail/comparison.hpp
@@ -10,67 +10,12 @@
 #include <bounded/detail/max_builtin.hpp>
 #include <bounded/detail/noexcept.hpp>
 #include <bounded/detail/requires.hpp>
+#include <bounded/detail/strong_ordering.hpp>
 
 #include <functional>
 #include <type_traits>
 
 namespace bounded {
-
-struct strong_ordering {
-	constexpr explicit strong_ordering(int value_) noexcept:
-		value(value_)
-	{
-	}
-
-	friend constexpr auto operator==(strong_ordering const lhs, std::nullptr_t) noexcept {
-		return lhs.value == 0;
-	}
-	friend constexpr auto operator==(std::nullptr_t, strong_ordering const rhs) noexcept {
-		return 0 == rhs.value;
-	}
-
-	friend constexpr auto operator!=(strong_ordering const lhs, std::nullptr_t) noexcept {
-		return lhs.value != 0;
-	}
-	friend constexpr auto operator!=(std::nullptr_t, strong_ordering const rhs) noexcept {
-		return 0 != rhs.value;
-	}
-
-	friend constexpr auto operator<(strong_ordering const lhs, std::nullptr_t) noexcept {
-		return lhs.value < 0;
-	}
-	friend constexpr auto operator<(std::nullptr_t, strong_ordering const rhs) noexcept {
-		return 0 < rhs.value;
-	}
-
-	friend constexpr auto operator>(strong_ordering const lhs, std::nullptr_t) noexcept {
-		return lhs.value > 0;
-	}
-	friend constexpr auto operator>(std::nullptr_t, strong_ordering const rhs) noexcept {
-		return 0 > rhs.value;
-	}
-
-	friend constexpr auto operator<=(strong_ordering const lhs, std::nullptr_t) noexcept {
-		return lhs.value <= 0;
-	}
-	friend constexpr auto operator<=(std::nullptr_t, strong_ordering const rhs) noexcept {
-		return 0 <= rhs.value;
-	}
-
-	friend constexpr auto operator>=(strong_ordering const lhs, std::nullptr_t) noexcept {
-		return lhs.value >= 0;
-	}
-	friend constexpr auto operator>=(std::nullptr_t, strong_ordering const rhs) noexcept {
-		return 0 >= rhs.value;
-	}
-
-private:
-	int value;
-};
-
-constexpr auto strong_ordering_less = strong_ordering(-1);
-constexpr auto strong_ordering_equal = strong_ordering(0);
-constexpr auto strong_ordering_greater = strong_ordering(1);
 
 template<typename LHS, typename RHS>
 constexpr auto compare(LHS * const lhs, RHS * const rhs) noexcept {
