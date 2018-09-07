2633ace7e3e536befeef350fe59384bdbe9fa3e6
David Stone <david@doublewise.net>
2018-09-06 18:35:26 -0600

    Added constexpr reference_wrapper.

---
+++ include/containers/reference_wrapper.hpp
@@ -0,0 +1,55 @@
+// Copyright David Stone 2018.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <bounded/detail/forward.hpp>
+#include <bounded/detail/noexcept.hpp>
+#include <utility>
+
+namespace containers {
+
+// Until the standard has a constexpr version
+template<typename T>
+struct reference_wrapper {
+	using type = T;
+
+	constexpr reference_wrapper(T & ref) noexcept:
+		m_ptr(std::addressof(ref))
+	{
+	}
+
+	constexpr operator T &() const noexcept {
+		return *m_ptr;
+	}
+	constexpr T & get() const noexcept {
+		return *m_ptr;
+	}
+
+	template<typename... Args>
+	constexpr auto operator()(Args && ... args) const BOUNDED_NOEXCEPT_DECLTYPE(
+		get()(BOUNDED_FORWARD(args)...)
+	)
+
+private:
+	T * m_ptr;
+};
+
+template<typename T>
+reference_wrapper(T &) -> reference_wrapper<T>;
+
+template<typename T>
+constexpr auto ref(T & value) noexcept {
+	return reference_wrapper<T>(value);
+}
+
+template<typename T>
+constexpr auto cref(T const & value) noexcept {
+	return reference_wrapper<T const>(value);
+}
+template<typename T>
+constexpr auto cref(T && value) = delete;
+
+}	// namespace containers
