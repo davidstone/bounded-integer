21ca36b5c349e0b8291adc4cd5ad69b3e04f410e
David Stone <david@doublewise.net>
2018-09-14 16:18:54 -0600

    Do not require is_container for append, just use syntactic checks.

--- include/containers/append.hpp
+++ include/containers/append.hpp
@@ -1,4 +1,4 @@
-// Copyright David Stone 2016.
+// Copyright David Stone 2018.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -6,7 +6,6 @@
 #pragma once
 
 #include <containers/begin_end.hpp>
-#include <containers/is_container.hpp>
 #include <containers/is_iterator_sentinel.hpp>
 #include <containers/push_back.hpp>
 #include <containers/size.hpp>
@@ -15,10 +14,21 @@
 
 namespace containers {
 namespace detail {
+
+template<typename, typename = void>
+constexpr auto has_push_back = false;
+
+template<typename Container>
+constexpr auto has_push_back<
+	Container,
+	std::void_t<decltype(push_back(std::declval<Container &>(), std::declval<typename Container::value_type>()))>
+> = true;
+
 namespace common {
 
 // TODO: specialize for ForwardIterator to call reserve
-template<typename Container, typename InputIterator, typename Sentinel, BOUNDED_REQUIRES(is_container<Container> and is_iterator_sentinel<InputIterator, Sentinel>)>
+// TODO: work with non-sized containers
+template<typename Container, typename InputIterator, typename Sentinel, BOUNDED_REQUIRES(has_push_back<Container> and is_iterator_sentinel<InputIterator, Sentinel>)>
 constexpr auto append(Container & container, InputIterator first, Sentinel last) {
 	auto const offset = size(container);
 	for (; first != last; ++first) {
