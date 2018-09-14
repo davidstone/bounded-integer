d55ed232ae467e1f35cd34fb1e13c8dc571580a7
David Stone <david@doublewise.net>
2018-09-14 16:17:59 -0600

    Do not require is_container for push_back, just use syntactic checks.

--- include/containers/push_back.hpp
+++ include/containers/push_back.hpp
@@ -1,11 +1,9 @@
-// Copyright David Stone 2016.
+// Copyright David Stone 2018.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
-
-#include <containers/is_container.hpp>
 
 #include <bounded/integer.hpp>
 
@@ -16,28 +14,28 @@ namespace detail {
 namespace common {
 
 template<typename, typename = void>
-constexpr auto has_push_back = false;
+constexpr auto has_member_push_back = false;
 
 template<typename Container>
-constexpr auto has_push_back<
+constexpr auto has_member_push_back<
 	Container,
-	std::void_t<decltype(std::declval<Container>().push_back(std::declval<typename Container::value_type>()))>
+	std::void_t<decltype(std::declval<Container &>().push_back(std::declval<typename Container::value_type>()))>
 > = true;
 
-template<typename Container, BOUNDED_REQUIRES(is_container<Container> and !has_push_back<Container>)>
+template<typename Container, BOUNDED_REQUIRES(!has_member_push_back<Container>)>
 constexpr auto push_back(Container & container, typename Container::value_type const & value) BOUNDED_NOEXCEPT_DECLTYPE(
 	container.emplace_back(value)
 )
-template<typename Container, BOUNDED_REQUIRES(is_container<Container> and !has_push_back<Container>)>
+template<typename Container, BOUNDED_REQUIRES(!has_member_push_back<Container>)>
 constexpr auto push_back(Container & container, typename Container::value_type && value) BOUNDED_NOEXCEPT_DECLTYPE(
 	container.emplace_back(std::move(value))
 )
 
-template<typename Container, BOUNDED_REQUIRES(is_container<Container> and has_push_back<Container>)>
+template<typename Container, BOUNDED_REQUIRES(has_member_push_back<Container>)>
 constexpr auto push_back(Container & container, typename Container::value_type const & value) BOUNDED_NOEXCEPT_DECLTYPE(
 	container.push_back(value)
 )
-template<typename Container, BOUNDED_REQUIRES(is_container<Container> and has_push_back<Container>)>
+template<typename Container, BOUNDED_REQUIRES(has_member_push_back<Container>)>
 constexpr auto push_back(Container & container, typename Container::value_type && value) BOUNDED_NOEXCEPT_DECLTYPE(
 	container.push_back(std::move(value))
 )
