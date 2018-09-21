55bb77a32bf753504226ece708b7b88b733e2d62
David Stone <david@doublewise.net>
2018-09-21 17:32:23 -0600

    Do not use tuple in the scope_guard implementation.

--- include/containers/scope_guard.hpp
+++ include/containers/scope_guard.hpp
@@ -5,45 +5,38 @@
 
 #pragma once
 
-#include <containers/tuple.hpp>
-
-#include <bounded/integer.hpp>
-
 #include <type_traits>
 #include <utility>
 
 namespace containers {
-using namespace bounded::literal;
 
 template<typename Function>
 struct scope_guard {
 	constexpr explicit scope_guard(Function f) noexcept(std::is_nothrow_move_constructible_v<Function>):
-		m_data(std::move(f), true)
+		m_function(std::move(f)),
+		m_is_active(true)
 	{
 	}
 	
 	constexpr scope_guard(scope_guard && other) noexcept(std::is_nothrow_move_constructible_v<Function>):
-		m_data(
-			std::move(other.m_data[function]),
-			std::exchange(other.m_data[is_active], false)
-		)
+		m_function(std::move(other.m_function)),
+		m_is_active(std::exchange(other.m_is_active, false))
 	{
 	}
 
 	~scope_guard() {
-		if (m_data[is_active]) {
-			std::move(m_data)[function]();
+		if (m_is_active) {
+			std::move(m_function)();
 		}
 	}
 	
 	constexpr void dismiss() noexcept {
-		m_data[is_active] = false;
+		m_is_active = false;
 	}
 
 private:
-	static inline constexpr auto function = 0_bi;
-	static inline constexpr auto is_active = 1_bi;
-	tuple<Function, bool> m_data;
+	Function m_function;
+	bool m_is_active;
 };
 
 }	// namespace containers
