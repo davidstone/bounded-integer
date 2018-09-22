0e6132417355ec2c15535a908af2a45d32c32d79
David Stone <david@doublewise.net>
2018-09-21 19:09:08 -0600

    Added tests that optional is properly trivial.

--- source/bounded/detail/optional/optional.cpp
+++ source/bounded/detail/optional/optional.cpp
@@ -1,16 +1,28 @@
-// Copyright David Stone 2015.
+// Copyright David Stone 2018.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <bounded/detail/optional/optional.hpp>
 
+#include <bounded/integer.hpp>
+
 #include <type_traits>
 
 namespace {
 
-using type = int;
+template<typename T>
+constexpr auto test_optional() {
+	using bounded::optional;
+	static_assert(std::is_convertible_v<T, optional<T>>);
+	static_assert(std::is_trivially_destructible_v<T> == std::is_trivially_destructible_v<optional<T>>);
+	static_assert(std::is_trivially_copy_constructible_v<T> == std::is_trivially_copy_constructible_v<T>);
+	static_assert(std::is_trivially_copy_assignable_v<T> == std::is_trivially_copy_assignable_v<T>);
+	return true;
+}
 
-static_assert(std::is_convertible<type, bounded::optional<type>>{}, "Cannot construct optional of identical type.");
+static_assert(test_optional<int>());
+static_assert(test_optional<bounded::integer<0, 10>>());
+static_assert(test_optional<decltype(bounded::integer(0))>());
 
 }	// namespace
