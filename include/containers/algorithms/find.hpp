82b5e481bb1d1d87a0d0ebdff6dd32ff6fcacde5
David Stone <david@doublewise.net>
2018-09-28 19:31:00 -0600

    Renamed find_pointer to maybe_find. Added support for iterators that return by value.

--- include/containers/algorithms/find.hpp
+++ include/containers/algorithms/find.hpp
@@ -8,6 +8,7 @@
 #include <containers/algorithms/negate.hpp>
 
 #include <bounded/integer.hpp>
+#include <bounded/optional.hpp>
 
 #include <type_traits>
 
@@ -52,34 +53,43 @@ constexpr auto find(Range && range, T const & value) BOUNDED_NOEXCEPT_VALUE(
 
 namespace detail {
 
-template<typename InputIterator, typename Sentinel>
-constexpr auto find_pointer_if_helper(InputIterator const it, Sentinel const last) BOUNDED_NOEXCEPT_VALUE(
+template<typename ForwardIterator, typename Sentinel, BOUNDED_REQUIRES(
+	std::is_lvalue_reference_v<decltype(*std::declval<ForwardIterator>())>
+)>
+constexpr auto maybe_find_if_helper(ForwardIterator const it, Sentinel const last) BOUNDED_NOEXCEPT_VALUE(
 	it != last ? std::addressof(*it) : nullptr
+)
+
+template<typename ForwardIterator, typename Sentinel, BOUNDED_REQUIRES(
+	!std::is_lvalue_reference_v<decltype(*std::declval<ForwardIterator>())>
+)>
+constexpr auto maybe_find_if_helper(ForwardIterator const it, Sentinel const last) BOUNDED_NOEXCEPT_VALUE(
+	it != last ? bounded::make_optional(*it) : bounded::none
 )
 
 } // namespace detail
 
-// the find_pointer functions return a pointer to the found element if it
-// exists, otherwise it returns a nullptr.
-template<typename InputIterator, typename Sentinel, typename UnaryPredicate>
-constexpr auto find_pointer_if(InputIterator const first, Sentinel const last, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
-	::containers::detail::find_pointer_if_helper(::containers::find_if(first, last, std::move(p)), last)
+// the maybe_find functions return a value contextually convertible to bool. If
+// the range contains the element, it can be dereferenced as an optional
+template<typename ForwardIterator, typename Sentinel, typename UnaryPredicate>
+constexpr auto maybe_find_if(ForwardIterator const first, Sentinel const last, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
+	::containers::detail::maybe_find_if_helper(::containers::find_if(first, last, std::move(p)), last)
 )
 
 template<typename Range, typename UnaryPredicate>
-constexpr auto find_pointer_if(Range && range, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
-	::containers::find_pointer_if(begin(BOUNDED_FORWARD(range)), end(BOUNDED_FORWARD(range)), std::move(p))
+constexpr auto maybe_find_if(Range && range, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
+	::containers::maybe_find_if(begin(BOUNDED_FORWARD(range)), end(BOUNDED_FORWARD(range)), std::move(p))
 )
 
 
-template<typename InputIterator, typename Sentinel, typename T>
-constexpr auto find_pointer(InputIterator const first, Sentinel const last, T const & value) BOUNDED_NOEXCEPT_VALUE(
-	::containers::find_pointer_if(first, last, bounded::equal_to(value))
+template<typename ForwardIterator, typename Sentinel, typename T>
+constexpr auto maybe_find(ForwardIterator const first, Sentinel const last, T const & value) BOUNDED_NOEXCEPT_VALUE(
+	::containers::maybe_find_if(first, last, bounded::equal_to(value))
 )
 
 template<typename Range, typename T>
-constexpr auto find_pointer(Range && range, T const & value) BOUNDED_NOEXCEPT_VALUE(
-	::containers::find_pointer(begin(BOUNDED_FORWARD(range)), end(BOUNDED_FORWARD(range)), value)
+constexpr auto maybe_find(Range && range, T const & value) BOUNDED_NOEXCEPT_VALUE(
+	::containers::maybe_find(begin(BOUNDED_FORWARD(range)), end(BOUNDED_FORWARD(range)), value)
 )
 
 
--- source/containers/algorithms/find.cpp
+++ source/containers/algorithms/find.cpp
@@ -12,8 +12,8 @@ using namespace bounded::literal;
 
 constexpr auto data = containers::array{1, 2, 5, 9, 0};
 
-static_assert(containers::find_pointer(data, 5) == std::addressof(data[2_bi]));
-static_assert(!containers::find_pointer(data, 7));
-static_assert(containers::find_pointer_if(data, [](auto const x) { return x % 2 == 0; }) == std::addressof(data[1_bi]));
+static_assert(containers::maybe_find(data, 5) == std::addressof(data[2_bi]));
+static_assert(!containers::maybe_find(data, 7));
+static_assert(containers::maybe_find_if(data, [](auto const x) { return x % 2 == 0; }) == std::addressof(data[1_bi]));
 
 } // namespace
