b65b0d6226c35b2d7d1672b085a606023a56e34c
David Stone <david@doublewise.net>
2018-11-02 17:47:03 -0600

    Moved maybe_find into its own header.

---
+++ include/containers/algorithms/maybe_find.hpp
@@ -0,0 +1,58 @@
+// Copyright David Stone 2018.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <containers/algorithms/find.hpp>
+
+#include <bounded/integer.hpp>
+#include <bounded/optional.hpp>
+
+#include <type_traits>
+#include <utility>
+
+namespace containers {
+namespace detail {
+
+template<typename ForwardIterator, typename Sentinel, BOUNDED_REQUIRES(
+	std::is_lvalue_reference_v<decltype(*std::declval<ForwardIterator>())>
+)>
+constexpr auto maybe_find_if_helper(ForwardIterator const it, Sentinel const last) BOUNDED_NOEXCEPT_VALUE(
+	it != last ? std::addressof(*it) : nullptr
+)
+
+template<typename ForwardIterator, typename Sentinel, BOUNDED_REQUIRES(
+	!std::is_lvalue_reference_v<decltype(*std::declval<ForwardIterator>())>
+)>
+constexpr auto maybe_find_if_helper(ForwardIterator const it, Sentinel const last) BOUNDED_NOEXCEPT_VALUE(
+	it != last ? bounded::make_optional(*it) : bounded::none
+)
+
+} // namespace detail
+
+// the maybe_find functions return a value contextually convertible to bool. If
+// the range contains the element, it can be dereferenced as an optional
+template<typename ForwardIterator, typename Sentinel, typename UnaryPredicate>
+constexpr auto maybe_find_if(ForwardIterator const first, Sentinel const last, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
+	::containers::detail::maybe_find_if_helper(::containers::find_if(first, last, std::move(p)), last)
+)
+
+template<typename Range, typename UnaryPredicate>
+constexpr auto maybe_find_if(Range && range, UnaryPredicate p) BOUNDED_NOEXCEPT_VALUE(
+	::containers::maybe_find_if(begin(BOUNDED_FORWARD(range)), end(BOUNDED_FORWARD(range)), std::move(p))
+)
+
+
+template<typename ForwardIterator, typename Sentinel, typename T>
+constexpr auto maybe_find(ForwardIterator const first, Sentinel const last, T const & value) BOUNDED_NOEXCEPT_VALUE(
+	::containers::maybe_find_if(first, last, bounded::equal_to(value))
+)
+
+template<typename Range, typename T>
+constexpr auto maybe_find(Range && range, T const & value) BOUNDED_NOEXCEPT_VALUE(
+	::containers::maybe_find(begin(BOUNDED_FORWARD(range)), end(BOUNDED_FORWARD(range)), value)
+)
+
+}	// namespace containers
--- source/containers/algorithms/find.cpp
+++ source/containers/algorithms/find.cpp
@@ -4,16 +4,3 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/algorithms/find.hpp>
-
-#include <containers/array/array.hpp>
-
-namespace {
-using namespace bounded::literal;
-
-constexpr auto data = containers::array{1, 2, 5, 9, 0};
-
-static_assert(containers::maybe_find(data, 5) == std::addressof(data[2_bi]));
-static_assert(!containers::maybe_find(data, 7));
-static_assert(containers::maybe_find_if(data, [](auto const x) { return x % 2 == 0; }) == std::addressof(data[1_bi]));
-
-} // namespace
