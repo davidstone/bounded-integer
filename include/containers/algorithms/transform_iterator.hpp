b053b0cfb26846633059462f7de2405b6acbaeba
David Stone <david@doublewise.net>
2019-02-16 13:24:29 -0700

    Support passing invokables to transform_iterator.

--- include/containers/algorithms/transform_iterator.hpp
+++ include/containers/algorithms/transform_iterator.hpp
@@ -6,41 +6,99 @@
 #pragma once
 
 #include <containers/iterator_adapter.hpp>
+#include <containers/reference_wrapper.hpp>
+
+#include <functional>
 
 namespace containers {
 namespace detail {
 
 // TODO: [[no_unique_address]]
-template<typename UnaryFunction>
-struct transform_traits : default_add, default_subtract, default_compare, private UnaryFunction {
-	transform_traits() = default;
-	constexpr explicit transform_traits(UnaryFunction dereference):
-		UnaryFunction(std::move(dereference))
-	{
-	}
-	
-	template<typename Iterator>
-	constexpr auto dereference(Iterator const it) const
-		noexcept(noexcept(std::declval<UnaryFunction const &>()(*it)))
-		-> decltype(std::declval<UnaryFunction const &>()(*it))
-	{
-		return static_cast<UnaryFunction const &>(*this)(*it);
+// TODO: std::invoke
+
+template<typename T>
+constexpr auto is_reference_wrapper = false;
+
+template<typename T>
+constexpr auto is_reference_wrapper<reference_wrapper<T>> = true;
+
+template<typename T>
+constexpr auto is_reference_wrapper<std::reference_wrapper<T>> = true;
+
+template<typename T, bool should_derive = std::is_class_v<T> and !std::is_final_v<T> and std::is_empty_v<T>>
+struct function_wrapper;
+
+template<typename F>
+struct function_wrapper<F, true> : F {
+	using F::operator();
+};
+
+template<typename F>
+struct function_wrapper<F, false> {
+	F function;
+
+	template<typename... Args>
+	constexpr auto operator()(Args && ... args) const BOUNDED_NOEXCEPT_DECLTYPE(
+		  function(BOUNDED_FORWARD(args)...)
+	)
+};
+
+template<typename Result, typename Type>
+struct function_wrapper<Result Type::*, false> {
+	Result Type::* function;
+
+	template<typename T1, typename... Args>
+	constexpr auto operator()(T1 && t1, Args && ... args) const noexcept(std::is_nothrow_invocable_v<decltype(function), T1, Args...>) -> std::invoke_result_t<decltype(function), T1, Args...> {
+		if constexpr (is_reference_wrapper<std::decay_t<T1>>) {
+			return operator()(function, t1.get(), BOUNDED_FORWARD(args)...);
+		} else if constexpr (std::is_member_function_pointer_v<decltype(function)>) {
+			if constexpr (std::is_base_of_v<Type, std::decay_t<T1>>) {
+				return (BOUNDED_FORWARD(t1).*function)(BOUNDED_FORWARD(args)...);
+			} else {
+				return ((*BOUNDED_FORWARD(t1)).*function)(BOUNDED_FORWARD(args)...);
+			}
+		} else {
+			static_assert(std::is_member_object_pointer_v<decltype(function)>);
+			static_assert(sizeof...(args) == 0);
+			if constexpr (std::is_base_of_v<Type, std::decay_t<T1>>) {
+				return BOUNDED_FORWARD(t1).*function;
+			} else {
+				return (*BOUNDED_FORWARD(t1)).*function;
+			}
+		}
 	}
 };
 
 template<typename UnaryFunction>
-struct transform_traits_dereference : default_add, default_subtract, default_compare, private UnaryFunction {
-	constexpr explicit transform_traits_dereference(UnaryFunction dereference):
-		UnaryFunction(std::move(dereference))
+struct transform_traits : default_add, default_subtract, default_compare, private function_wrapper<UnaryFunction> {
+	transform_traits() = default;
+	constexpr explicit transform_traits(UnaryFunction dereference):
+		function_wrapper<UnaryFunction>{std::move(dereference)}
 	{
 	}
 	
 	template<typename Iterator>
 	constexpr auto dereference(Iterator const it) const
-		noexcept(noexcept(std::declval<UnaryFunction const &>()(it)))
-		-> decltype(std::declval<UnaryFunction const &>()(it))
+		noexcept(noexcept(std::declval<function_wrapper<UnaryFunction> const &>()(*it)))
+		-> decltype(std::declval<function_wrapper<UnaryFunction> const &>()(*it))
 	{
-		return static_cast<UnaryFunction const &>(*this)(it);
+		return static_cast<function_wrapper<UnaryFunction> const &>(*this)(*it);
+	}
+};
+
+template<typename UnaryFunction>
+struct transform_traits_dereference : default_add, default_subtract, default_compare, private function_wrapper<UnaryFunction> {
+	constexpr explicit transform_traits_dereference(UnaryFunction dereference):
+		function_wrapper<UnaryFunction>{std::move(dereference)}
+	{
+	}
+	
+	template<typename Iterator>
+	constexpr auto dereference(Iterator const it) const
+		noexcept(noexcept(std::declval<function_wrapper<UnaryFunction> const &>()(it)))
+		-> decltype(std::declval<function_wrapper<UnaryFunction> const &>()(it))
+	{
+		return static_cast<function_wrapper<UnaryFunction> const &>(*this)(it);
 	}
 };
 
--- source/containers/algorithms/transform.cpp
+++ source/containers/algorithms/transform.cpp
@@ -1,6 +1,39 @@
-// Copyright David Stone 2018.
+// Copyright David Stone 2019.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/algorithms/transform.hpp>
+#include <containers/algorithms/compare.hpp>
+#include <containers/array/array.hpp>
+
+namespace {
+
+struct S {
+	constexpr auto f() const { return n; }
+	int n;
+};
+
+constexpr auto operator==(S const lhs, S const rhs) {
+	return lhs.n == rhs.n;
+};
+
+constexpr auto array = containers::array{S{0}, S{1}, S{2}, S{3}, S{4}};
+
+static_assert(containers::equal(
+	containers::transform(array, [](S const s) { return s; }),
+	array
+));
+
+constexpr auto integer_array = containers::array{0, 1, 2, 3, 4};
+static_assert(containers::equal(
+	containers::transform(array, &S::f),
+	integer_array
+));
+
+static_assert(containers::equal(
+	containers::transform(array, [](S const s) { return s.f(); }),
+	integer_array
+));
+
+} // namespace
