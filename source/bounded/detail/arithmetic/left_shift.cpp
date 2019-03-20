e98994ae36df853e7f61676f0a72082f23cefeb8
David Stone <david@doublewise.net>
2019-03-20 09:23:17 -0600

    Fix to compile on Linux.

--- source/bounded/detail/arithmetic/left_shift.cpp
+++ source/bounded/detail/arithmetic/left_shift.cpp
@@ -11,7 +11,7 @@ namespace {
 
 static_assert(homogeneous_equals(
 	bounded::integer<0, 2>(1, bounded::non_check) << bounded::integer<0, 60>(3, bounded::non_check),
-	bounded::integer<0, 2LL << 60LL>(1 << 3, bounded::non_check)
+	bounded::integer<0, bounded::detail::normalize<2LL << 60LL>>(1 << 3, bounded::non_check)
 ));
 
 }	// namespace
