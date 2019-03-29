001cc58ebad09c5d18971a444ea2066de0c6f331
David Stone <david@doublewise.net>
2019-03-29 16:53:04 -0600

    Added BOUNDED_ASSUME.

---
+++ include/bounded/detail/assume.hpp
@@ -0,0 +1,14 @@
+// Copyright David Stone 2019.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#if defined __clang__
+	#define BOUNDED_ASSUME __builtin_assume
+#elif defined _MSC_VER
+	#define BOUNDED_ASSUME __assume
+#else
+	#define BOUNDED_ASSUME(...) static_cast<void>(sizeof(__VA_ARGS__))
+#endif
---
+++ source/bounded/detail/assume.cpp
@@ -0,0 +1,10 @@
+// Copyright David Stone 2019.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <bounded/detail/assume.hpp>
+
+#include <type_traits>
+
+static_assert(std::is_void_v<decltype(BOUNDED_ASSUME(true))>);
