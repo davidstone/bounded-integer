7209cb958acf383c8dd456088e9ac512bf08fb13
David Stone <david@doublewise.net>
2019-03-29 19:54:47 -0600

    Instantiate all typedefs in typedefs.cpp.

--- source/bounded/detail/typedefs.cpp
+++ source/bounded/detail/typedefs.cpp
@@ -1,6 +1,14 @@
-// Copyright David Stone 2015.
+// Copyright David Stone 2019.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <bounded/detail/typedefs.hpp>
+
+namespace {
+
+static_assert(bounded::checked_integer<0, 0>(bounded::constant<0>) == bounded::constant<0>);
+static_assert(bounded::clamped_integer<0, 0>(bounded::constant<0>) == bounded::constant<0>);
+static_assert(bounded::wrapping_integer<0, 0>(bounded::constant<0>) == bounded::constant<0>);
+
+} // namespace
