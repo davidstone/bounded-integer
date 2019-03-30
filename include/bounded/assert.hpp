02f474bd427e1e0dca593be64cea35fc62c129ab
David Stone <david@doublewise.net>
2019-03-29 19:43:03 -0600

    Split BOUNDED_ASSERT into BOUNDED_ASSERT and BOUNDED_ASSERT_OR_ASSUME.

--- include/bounded/assert.hpp
+++ include/bounded/assert.hpp
@@ -11,15 +11,13 @@
 #include <cstdlib>
 
 #if defined NDEBUG or defined BOUNDED_NDEBUG
-	#if defined BOUNDED_NEVER_ASSUME_ASSERTS
-		#define BOUNDED_ASSERT(...) void(sizeof(__VA_ARGS__))
-	#else
-		#define BOUNDED_ASSERT(...) BOUNDED_ASSUME(__VA_ARGS__)
-	#endif
+	#define BOUNDED_ASSERT(...) void(sizeof(__VA_ARGS__))
+	#define BOUNDED_ASSERT_OR_ASSUME BOUNDED_ASSUME
 #else
 	// This is not defined to just be assert to support expressions with commas,
 	// but is still carefully written to not defeat warnings for expressions
 	// like `assert(false, "My error message")` which actually would invoke the
 	// comma operator.
 	#define BOUNDED_ASSERT(...) (static_cast<bool>(__VA_ARGS__) ? void(0) : assert((__VA_ARGS__)))
+	#define BOUNDED_ASSERT_OR_ASSUME BOUNDED_ASSERT
 #endif
--- source/bounded/assert-NDEBUG.cpp
+++ source/bounded/assert-NDEBUG.cpp
@@ -3,9 +3,13 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#undef NDEBUG
 #define NDEBUG
-#undef BOUNDED_NEVER_ASSUME_ASSERTS
 
 #include <bounded/assert.hpp>
 
 static_assert([]{ BOUNDED_ASSERT(true); return true; }());
+static_assert([]{ BOUNDED_ASSERT(false); return true; }());
+static_assert([]{ BOUNDED_ASSERT_OR_ASSUME(true); return true; }());
+// static_assert([]{ BOUNDED_ASSERT_OR_ASSUME(false); return true; }());
+
