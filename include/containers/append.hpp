39a29e60340013a619d41a5e9e12321e76010ef1
David Stone <david@doublewise.net>
2019-05-14 17:58:14 -0600

    append now calls reserve when possible.

--- include/containers/append.hpp
+++ include/containers/append.hpp
@@ -7,6 +7,7 @@
 
 #include <containers/begin_end.hpp>
 #include <containers/push_back.hpp>
+#include <containers/reserve_if_reservable.hpp>
 #include <containers/size.hpp>
 
 #include <bounded/integer.hpp>
@@ -23,13 +24,32 @@ constexpr auto has_push_back<
 	std::void_t<decltype(push_back(std::declval<Container &>(), std::declval<typename Container::value_type>()))>
 > = true;
 
+template<typename, typename = void>
+constexpr auto has_size = false;
+
+template<typename Container>
+constexpr auto has_size<
+	Container,
+	std::void_t<decltype(size(std::declval<Container>()))>
+> = true;
+
 namespace common {
 
-// TODO: specialize for ForwardIterator to call reserve
-// TODO: work with non-sized containers
+// TODO: work with non-sized containers and non-random-access containers
 template<typename Container, typename Range, BOUNDED_REQUIRES(has_push_back<Container>)>
 constexpr auto append(Container & output, Range && input) {
 	auto const offset = size(output);
+	if constexpr (has_size<Range>) {
+		auto const input_size = [&] {
+			auto const value = size(input);
+			if constexpr (bounded::is_bounded_integer<decltype(value)>) {
+				return value;
+			} else {
+				return typename Container::size_type(value);
+			}
+		};
+		::containers::detail::growth_reallocation(output, input_size());
+	}
 	for (decltype(auto) value : BOUNDED_FORWARD(input)) {
 		push_back(output, BOUNDED_FORWARD(value));
 	}
