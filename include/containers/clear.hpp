d0cc06295e07551266f804e7beb696f614656b9c
David Stone <david@doublewise.net>
2019-05-15 18:15:06 -0600

    Implement clear more efficiently so that it does not deallocate all old capacity.

--- include/containers/clear.hpp
+++ include/containers/clear.hpp
@@ -11,11 +11,40 @@
 
 namespace containers {
 namespace detail {
+
+template<typename, typename = void>
+constexpr auto has_pop_back = false;
+
+template<typename Container>
+constexpr auto has_pop_back<
+	Container,
+	std::void_t<decltype(std::declval<Container &>().pop_back())>
+> = true;
+
+template<typename, typename = void>
+constexpr auto has_pop_front = false;
+
+template<typename Container>
+constexpr auto has_pop_front<
+	Container,
+	std::void_t<decltype(std::declval<Container &>().pop_front())>
+> = true;
+
 namespace common {
 
 template<typename Container, BOUNDED_REQUIRES(is_container<Container>)>
 constexpr auto clear(Container & container) noexcept {
-	container = Container{};
+	if constexpr (has_pop_back<Container>) {
+		while (!empty(container)) {
+			container.pop_back();
+		}
+	} else if constexpr (has_pop_front<Container>) {
+		while (!empty(container)) {
+			container.pop_front();
+		}
+	} else {
+		container = {};
+	}
 }
 
 }	// namespace common
