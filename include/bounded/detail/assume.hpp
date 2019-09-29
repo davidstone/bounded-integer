ace2c2ff7a09dea9c882d05a687598fa4263b5a4
David Stone <david@doublewise.net>
2019-09-29 11:57:42 -0600

    Add support for gcc to BOUNDED_ASSUME

--- include/bounded/detail/assume.hpp
+++ include/bounded/detail/assume.hpp
@@ -9,6 +9,13 @@
 	#define BOUNDED_ASSUME __builtin_assume
 #elif defined _MSC_VER
 	#define BOUNDED_ASSUME __assume
+#elif defined __GNUC__
+	#define BOUNDED_ASSUME(...) \
+        do { \
+            if (!(__VA_ARGS__)) { \
+                __builtin_unreachable(); \
+            } \
+        } while (false)
 #else
 	#define BOUNDED_ASSUME(...) void(sizeof(__VA_ARGS__))
 #endif
