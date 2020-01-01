7ab678ad1bb13b1c60ce56a4f553126abea92c5f
David Stone <david@doublewise.net>
2019-12-31 17:28:36 -0700

    Use types in implementation of underlying_type, rather than defining a new type wrapper.

--- include/bounded/detail/underlying_type.hpp
+++ include/bounded/detail/underlying_type.hpp
@@ -8,6 +8,7 @@
 #include <bounded/detail/comparison.hpp>
 #include <bounded/detail/int128.hpp>
 #include <bounded/detail/overlapping_range.hpp>
+#include <bounded/detail/type.hpp>
 
 namespace bounded {
 namespace detail {
@@ -17,41 +18,36 @@ constexpr auto range_fits_in_type(auto const minimum, auto const maximum) {
 	return value_fits_in_type<T>(minimum) and value_fits_in_type<T>(maximum);
 }
 
-template<typename T>
-struct type_c {
-	using type = T;
-};
-
 template<auto...>
 inline constexpr auto false_ = false;
 
 template<auto minimum, auto maximum>
 constexpr auto determine_type() {
 	if constexpr (range_fits_in_type<unsigned char>(minimum, maximum)) {
-		return type_c<unsigned char>{};
+		return types<unsigned char>{};
 	} else if constexpr (range_fits_in_type<signed char>(minimum, maximum)) {
-		return type_c<signed char>{};
+		return types<signed char>{};
 	} else if constexpr (range_fits_in_type<unsigned short>(minimum, maximum)) {
-		return type_c<unsigned short>{};
+		return types<unsigned short>{};
 	} else if constexpr (range_fits_in_type<signed short>(minimum, maximum)) {
-		return type_c<signed short>{};
+		return types<signed short>{};
 	} else if constexpr (range_fits_in_type<unsigned int>(minimum, maximum)) {
-		return type_c<unsigned int>{};
+		return types<unsigned int>{};
 	} else if constexpr (range_fits_in_type<signed int>(minimum, maximum)) {
-		return type_c<signed int>{};
+		return types<signed int>{};
 	} else if constexpr (range_fits_in_type<unsigned long>(minimum, maximum)) {
-		return type_c<unsigned long>{};
+		return types<unsigned long>{};
 	} else if constexpr (range_fits_in_type<signed long>(minimum, maximum)) {
-		return type_c<signed long>{};
+		return types<signed long>{};
 	} else if constexpr (range_fits_in_type<unsigned long long>(minimum, maximum)) {
-		return type_c<unsigned long long>{};
+		return types<unsigned long long>{};
 	} else if constexpr (range_fits_in_type<signed long long>(minimum, maximum)) {
-		return type_c<signed long long>{};
+		return types<signed long long>{};
 #if defined BOUNDED_DETAIL_HAS_128_BIT
 	} else if constexpr (range_fits_in_type<uint128_t>(minimum, maximum)) {
-		return type_c<uint128_t>{};
+		return types<uint128_t>{};
 	} else if constexpr (range_fits_in_type<int128_t>(minimum, maximum)) {
-		return type_c<int128_t>{};
+		return types<int128_t>{};
 #endif
 	} else {
 		static_assert(false_<minimum, maximum>, "Bounds cannot fit in any type.");
