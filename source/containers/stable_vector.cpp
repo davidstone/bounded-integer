99ef5dbd623b9fae73c2de157c952fcc985b062f
David Stone <david@doublewise.net>
2020-02-22 15:43:34 -0700

    Run stable_vector unit tests at compile time

--- source/containers/stable_vector.cpp
+++ source/containers/stable_vector.cpp
@@ -15,7 +15,7 @@ namespace {
 using namespace bounded::literal;
 
 template<std::size_t capacity_, typename T>
-void test_generic(T const & t, std::initializer_list<T> init) {
+constexpr bool test_generic(T const & t, std::initializer_list<T> init) {
 	auto const capacity = bounded::constant<capacity_>;
 	using container = containers::stable_vector<T, capacity_>;
 	auto const default_constructed = container{};
@@ -41,11 +41,6 @@ void test_generic(T const & t, std::initializer_list<T> init) {
 	BOUNDED_TEST(back(count_arg) == t);
 	BOUNDED_TEST(count_arg[0_bi] == t);
 	BOUNDED_TEST(at(count_arg, 0_bi) == t);
-	try {
-		at(count_arg, static_cast<unsigned>(capacity + 1_bi));
-		BOUNDED_TEST(false);
-	} catch (std::out_of_range const &) {
-	}
 
 	auto const init_list = container(init);
 	BOUNDED_TEST(containers::equal(init_list, init));
@@ -99,41 +94,52 @@ void test_generic(T const & t, std::initializer_list<T> init) {
 	BOUNDED_TEST(empty(copy));
 	resize(copy, capacity, t);
 	BOUNDED_TEST(copy == count_arg);
+
+	return true;
 }
 
-struct non_copyable {
-	non_copyable() {}
-	non_copyable(non_copyable const &) = delete;
-	non_copyable(non_copyable &&) = default;
-	non_copyable & operator=(non_copyable const &) = delete;
-	non_copyable & operator=(non_copyable &&) = default;
+struct non_trivial_non_copyable {
+	constexpr non_trivial_non_copyable() {}
+	non_trivial_non_copyable(non_trivial_non_copyable const &) = delete;
+	non_trivial_non_copyable(non_trivial_non_copyable &&) = default;
+	non_trivial_non_copyable & operator=(non_trivial_non_copyable const &) = delete;
+	non_trivial_non_copyable & operator=(non_trivial_non_copyable &&) = default;
 };
 
-}	// namespace
+static_assert(test_generic<1>(0, { }));
+static_assert(test_generic<1>(0, { 5 }));
 
-int main() {
-	test_generic<1>(0, { });
-	test_generic<1>(0, { 5 });
+static_assert(test_generic<5>(0, { }));
+static_assert(test_generic<5>(9, { 0, 1, 4 }));
+static_assert(test_generic<5>(-4, { 0, 1, 2, 3, 4 }));
+static_assert(test_generic<3>(
+	containers::stable_vector<char, 100>(std::string_view("hi")),
+	{ containers::stable_vector<char, 100>(), containers::stable_vector<char, 100>(std::string_view("hello")), containers::stable_vector<char, 100>(std::string_view("asdf")) }
+));
 
-	test_generic<5>(0, { });
-	test_generic<5>(9, { 0, 1, 4 });
-	test_generic<5>(-4, { 0, 1, 2, 3, 4 });
+constexpr bool test_non_trivial_non_copyable() {
+	containers::stable_vector<non_trivial_non_copyable, 10> test_no_copies;
+	containers::emplace_back(test_no_copies);
+	insert(test_no_copies, begin(test_no_copies), non_trivial_non_copyable{});
+	return true;
+}
 
-	test_generic<3>(std::string("hi"), { std::string(""), std::string("hello"), std::string(100, '=') });
-	
+static_assert(test_non_trivial_non_copyable());
+
+static_assert(!containers::iterator<containers::stable_vector<std::string, 6>>);
+static_assert(containers::is_container<containers::stable_vector<std::string, 6>>);
+
+using index_type = containers::index_type<containers::stable_vector<int, 10>>;
+static_assert(bounded::min_value<index_type> == 0_bi);
+static_assert(bounded::max_value<index_type> == 9_bi);
+static_assert(std::is_same<index_type::overflow_policy, bounded::throw_policy<std::out_of_range>>{});
+
+constexpr bool check_insert_in_middle() {
 	containers::stable_vector<int, 10> container = { 1, 2, 3 };
-	using index_type = containers::index_type<decltype(container)>;
-	static_assert(bounded::min_value<index_type> == 0_bi);
-	static_assert(bounded::max_value<index_type> == 9_bi);
-	static_assert(std::is_same<index_type::overflow_policy, bounded::throw_policy<std::out_of_range>>{});
-	static_assert(!containers::iterator<containers::stable_vector<std::string, 6>>);
-	static_assert(containers::is_container<containers::stable_vector<std::string, 6>>);
-
 	containers::insert(container, begin(container) + 1_bi, containers::repeat_n(5_bi, 12));
 	auto const expected = { 1, 12, 12, 12, 12, 12, 2, 3 };
-	BOUNDED_TEST(containers::equal(container, expected));
-	
-	containers::stable_vector<non_copyable, 10> test_no_copies;
-	containers::emplace_back(test_no_copies);
-	insert(test_no_copies, begin(test_no_copies), non_copyable{});
+	return containers::equal(container, expected);
 }
+static_assert(check_insert_in_middle());
+
+} // namespace
