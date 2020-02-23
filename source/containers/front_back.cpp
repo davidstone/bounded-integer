0bdde75e328239a61620db674d5d91acf748f828
David Stone <david@doublewise.net>
2020-02-23 12:29:40 -0700

    Added tests for front and back

--- source/containers/front_back.cpp
+++ source/containers/front_back.cpp
@@ -1,6 +1,18 @@
-// Copyright David Stone 2016.
+// Copyright David Stone 2020.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/front_back.hpp>
+
+#include <containers/array/array.hpp>
+
+namespace {
+
+static_assert(front(containers::array{5}) == 5);
+static_assert(back(containers::array{5}) == 5);
+
+static_assert(front(containers::array{2, 3, 4}) == 2);
+static_assert(back(containers::array{2, 3, 4}) == 4);
+
+} // namespace
