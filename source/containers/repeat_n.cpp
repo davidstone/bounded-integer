c6bc361e04a1f69f77aba55bd59f8bb7e152e948
David Stone <david@doublewise.net>
2020-02-23 11:57:57 -0700

    Added unit tests to repeat_n.

--- source/containers/repeat_n.cpp
+++ source/containers/repeat_n.cpp
@@ -4,3 +4,17 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/repeat_n.hpp>
+
+#include <containers/algorithms/compare.hpp>
+#include <containers/array/array.hpp>
+
+namespace {
+
+using namespace bounded::literal;
+
+static_assert(containers::equal(containers::repeat_default_n<int>(0_bi), containers::array<int, 0>{}));
+static_assert(containers::equal(containers::repeat_default_n<int>(5_bi), containers::array{0, 0, 0, 0, 0}));
+static_assert(containers::equal(containers::repeat_n(0_bi, 3), containers::array<int, 0>{}));
+static_assert(containers::equal(containers::repeat_n(2_bi, 3), containers::array{3, 3}));
+
+} // namespace
