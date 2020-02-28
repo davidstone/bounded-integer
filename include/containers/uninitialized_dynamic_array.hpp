189725461aa964eea34e91b5d7fc65b09b61cb23
David Stone <david@doublewise.net>
2020-02-27 19:58:34 -0700

    Add `uninitialized_dynamic_array`

---
+++ include/containers/uninitialized_dynamic_array.hpp
@@ -0,0 +1,74 @@
+// Copyright David Stone 2020.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <bounded/assert.hpp>
+#include <bounded/concepts.hpp>
+#include <bounded/integer.hpp>
+
+#include <memory>
+
+namespace containers {
+
+using namespace bounded::literal;
+
+template<typename T, typename Capacity>
+struct uninitialized_dynamic_array {
+	constexpr uninitialized_dynamic_array() noexcept requires bounded::constructible_from<Capacity, bounded::constant_t<0>>:
+		m_ptr(nullptr),
+		m_capacity(0_bi)
+	{
+	}
+	constexpr explicit uninitialized_dynamic_array(Capacity capacity):
+		m_ptr(std::allocator<T>().allocate(static_cast<std::size_t>(capacity))),
+		m_capacity(capacity)
+	{
+	}
+	constexpr uninitialized_dynamic_array(uninitialized_dynamic_array && other) noexcept:
+		m_ptr(other.release()),
+		m_capacity(other.m_capacity)
+	{
+	}
+	constexpr uninitialized_dynamic_array & operator=(uninitialized_dynamic_array && other) & noexcept {
+		deallocate();
+		m_ptr = nullptr;
+		m_ptr = other.release();
+		m_capacity = other.m_capacity;
+		return *this;
+	}
+	constexpr ~uninitialized_dynamic_array() noexcept {
+		deallocate();
+	}
+	friend constexpr void swap(uninitialized_dynamic_array & lhs, uninitialized_dynamic_array & rhs) noexcept {
+		std::swap(lhs.m_ptr, rhs.m_ptr);
+		std::swap(lhs.m_capacity, rhs.m_capacity);
+	}
+
+	constexpr auto data() const noexcept -> T const * {
+		return m_ptr;
+	}
+	constexpr auto data() noexcept -> T * {
+		return m_ptr;
+	}
+	constexpr auto capacity() const noexcept {
+		BOUNDED_ASSERT(m_ptr != nullptr or m_capacity == 0_bi);
+		return m_capacity;
+	}
+
+private:
+	constexpr auto release() & noexcept {
+		return std::exchange(m_ptr, nullptr);
+	}
+	constexpr auto deallocate() & noexcept {
+		if (m_ptr) {
+			std::allocator<T>().deallocate(m_ptr, static_cast<std::size_t>(m_capacity));
+		}
+	}
+	T * m_ptr;
+	[[no_unique_address]] Capacity m_capacity;
+};
+
+} // namespace containers
---
+++ source/containers/uninitialized_dynamic_array.cpp
@@ -0,0 +1,47 @@
+// Copyright David Stone 2020.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <containers/uninitialized_dynamic_array.hpp>
+
+#include "test_sequence_container.hpp"
+
+namespace {
+
+using namespace bounded::literal;
+
+template<int max>
+using container = containers::uninitialized_dynamic_array<int, bounded::integer<0, max>>;
+
+static_assert(container<0>().capacity() == 0_bi);
+static_assert(container<1>().capacity() == 0_bi);
+
+constexpr auto basic_test() -> bool {
+    auto a = container<10>(1_bi);
+    auto b = container<10>(1_bi);
+    BOUNDED_TEST(a.data() != nullptr);
+    BOUNDED_TEST(b.data() != nullptr);
+    BOUNDED_TEST(a.data() != b.data());
+    BOUNDED_TEST(a.capacity() == 1_bi);
+    BOUNDED_TEST(b.capacity() == 1_bi);
+    return true;
+}
+static_assert(basic_test());
+
+constexpr auto move_construction() -> bool {
+    auto a = container<1>(1_bi);
+    auto const original_pointer = a.data();
+    auto b = std::move(a);
+    BOUNDED_TEST(a.data() == nullptr);
+    BOUNDED_TEST(b.data() == original_pointer);
+    BOUNDED_TEST(b.capacity() == 1_bi);
+    return true;
+}
+static_assert(move_construction());
+
+static_assert(containers_test::run_self_move_assignment<container<10>>(0_bi));
+static_assert(containers_test::run_self_move_assignment<container<10>>(1_bi));
+static_assert(containers_test::run_self_move_assignment<container<0>>(0_bi));
+
+} // namespace
