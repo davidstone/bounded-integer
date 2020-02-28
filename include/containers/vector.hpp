b56b25a004ec02ec9b1179563aeb6a7f88a2d13b
David Stone <david@doublewise.net>
2020-02-27 20:03:26 -0700

    Implement `vector<T>` in terms of `uninitialized_dynamic_array<T>` instead of `dynamic_array<trivial_storage<T>>`

--- include/containers/vector.hpp
+++ include/containers/vector.hpp
@@ -7,12 +7,11 @@
 
 #include <containers/common_container_functions.hpp>
 #include <containers/contiguous_iterator.hpp>
-#include <containers/dynamic_array.hpp>
-#include <containers/index_type.hpp>
 #include <containers/is_container.hpp>
-#include <containers/repeat_n.hpp>
-#include <containers/uninitialized_storage.hpp>
+#include <containers/maximum_array_size.hpp>
+#include <containers/uninitialized_dynamic_array.hpp>
 
+#include <operators/bracket.hpp>
 #include <operators/forward.hpp>
 
 #include <initializer_list>
@@ -24,7 +23,7 @@ namespace containers {
 template<typename T>
 struct vector {
 	using value_type = T;
-	using size_type = typename detail::dynamic_array_data<value_type>::size_type;
+	using size_type = detail::array_size_type<T>;
 
 	using const_iterator = contiguous_iterator<value_type const, bounded::detail::builtin_max_value<size_type>>;
 	using iterator = contiguous_iterator<value_type, bounded::detail::builtin_max_value<size_type>>;
@@ -35,48 +34,49 @@ struct vector {
 		!std::is_array_v<std::remove_cv_t<std::remove_reference_t<Range>>>
 	)
 	constexpr explicit vector(Range && source) {
-		append(*this, OPERATORS_FORWARD(source));
+		::containers::append(*this, OPERATORS_FORWARD(source));
 	}
 	
 	constexpr vector(std::initializer_list<value_type> init) {
-		append(*this, init);
+		::containers::append(*this, init);
 	}
 
+	// TODO: Support trivial relocatability
 	constexpr vector(vector && other) noexcept:
-		m_container(std::move(other.m_container)),
+		m_storage(std::move(other.m_storage)),
 		m_size(std::exchange(other.m_size, 0_bi))
 	{
 	}
 
 	constexpr vector(vector const & other) {
-		append(*this, other);
-	}
-
-	constexpr auto & operator=(vector && other) & noexcept {
-		::containers::detail::destroy_range(*this);
-		m_container = std::move(other.m_container);
-		m_size = std::exchange(other.m_size, 0_bi);
-		return *this;
-	}
-	constexpr auto & operator=(vector const & other) & {
-		assign(*this, other);
-		return *this;
-	}
-
-	constexpr auto & operator=(std::initializer_list<value_type> init) & {
-		assign(*this, init);
-		return *this;
+		::containers::append(*this, other);
 	}
 
 	constexpr ~vector() {
 		::containers::detail::destroy_range(*this);
 	}
 
+	constexpr auto & operator=(vector && other) & noexcept {
+		::containers::detail::destroy_range(*this);
+		m_storage = std::move(other.m_storage);
+		m_size = std::exchange(other.m_size, 0_bi);
+		return *this;
+	}
+	constexpr auto & operator=(vector const & other) & {
+		assign_range(other);
+		return *this;
+	}
+
+	constexpr auto & operator=(std::initializer_list<value_type> init) & {
+		assign_range(init);
+		return *this;
+	}
+
 	friend constexpr auto begin(vector const & container) {
-		return const_iterator(::containers::detail::static_or_reinterpret_cast<value_type const *>(data(container.m_container)));
+		return const_iterator(container.m_storage.data());
 	}
 	friend constexpr auto begin(vector & container) {
-		return iterator(::containers::detail::static_or_reinterpret_cast<value_type *>(data(container.m_container)));
+		return iterator(container.m_storage.data());
 	}
 	friend constexpr auto end(vector const & container) {
 		return begin(container) + container.m_size;
@@ -88,33 +88,38 @@ struct vector {
 	OPERATORS_BRACKET_SEQUENCE_RANGE_DEFINITIONS
 
 	constexpr auto capacity() const {
-		return ::containers::size(m_container);
+		return m_storage.capacity();
 	}
 	constexpr auto reserve(size_type const requested_capacity) {
 		if (requested_capacity <= capacity()) {
 			return;
 		}
-		auto temp = make_storage(requested_capacity);
+		auto temp = storage_type(requested_capacity);
 		containers::uninitialized_move_destroy(
 			*this,
 			temp.data()
 		);
-		m_container = std::move(temp);
+		m_storage = std::move(temp);
 		// m_size remains the same
 	}
 
 	// Assumes that elements are already constructed in the spare capacity
 	constexpr void append_from_capacity(auto const count) {
-		m_size = size(*this) + count;
+		BOUNDED_ASSERT(m_size + count <= capacity());
+		m_size += count;
 	}
 
 private:
-	constexpr auto make_storage(auto const new_capacity) {
-		return raw_container(repeat_default_n<typename raw_container::value_type>(new_capacity));
+	constexpr auto assign_range(auto && other) & {
+		if (!m_storage.data()) {
+			BOUNDED_ASSERT(m_size == 0_bi);
+			m_storage = storage_type();
+		}
+		assign(*this, other);
 	}
 	
-	using raw_container = dynamic_array<trivial_storage<value_type>>;
-	raw_container m_container;
+	using storage_type = uninitialized_dynamic_array<T, size_type>;
+	storage_type m_storage;
 	size_type m_size = 0_bi;
 };
 
