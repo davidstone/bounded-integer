8dd078b3c55b0acda80a07a91f8838ce40b12e85
David Stone <david@doublewise.net>
2020-02-27 20:13:32 -0700

    Added lazy_push_back unit tests

--- source/containers/lazy_push_back.cpp
+++ source/containers/lazy_push_back.cpp
@@ -4,3 +4,42 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/lazy_push_back.hpp>
+
+#include <containers/algorithms/compare.hpp>
+#include <containers/vector.hpp>
+
+#include "../test_assert.hpp"
+
+namespace {
+
+using namespace bounded::literal;
+
+constexpr bool test_lazy_push_back() {
+    auto c = containers::vector<int>();
+
+    BOUNDED_TEST(c.capacity() == 0_bi);
+
+    lazy_push_back(c, []{ return 3; });
+    BOUNDED_TEST(containers::equal(c, containers::vector{3}));
+
+    BOUNDED_TEST(c.capacity() == 1_bi);
+
+    lazy_push_back(c, []{ return 4; });
+    BOUNDED_TEST(containers::equal(c, containers::vector{3, 4}));
+
+    BOUNDED_TEST(c.capacity() == 2_bi);
+
+    lazy_push_back(c, []{ return 5; });
+    BOUNDED_TEST(containers::equal(c, containers::vector{3, 4, 5}));
+
+    BOUNDED_TEST(c.capacity() == 4_bi);
+
+    lazy_push_back(c, []{ return 12; });
+    BOUNDED_TEST(containers::equal(c, containers::vector{3, 4, 5, 12}));
+
+    return true;
+}
+
+static_assert(test_lazy_push_back());
+
+} // namespace
