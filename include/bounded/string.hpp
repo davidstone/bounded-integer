0a49ea51d93d2c4af322ff74e456db0b875584ad
David Stone <david@doublewise.net>
2020-12-09 17:46:57 -0700

    Implement `to_string` for `int128_t` and `uint128_t`

--- include/bounded/string.hpp
+++ include/bounded/string.hpp
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <bounded/detail/int128.hpp>
 #include <bounded/detail/is_bounded_integer.hpp>
 
 #include <string>
@@ -14,15 +15,32 @@ namespace bounded {
 // Import to_string for the numeric types
 using std::to_string;
 
+#if defined BOUNDED_DETAIL_HAS_128_BIT
+
+// TODO: Make this more efficient
+inline auto to_string(detail::uint128_t x) {
+	if (x == 0) {
+		return std::string({'0'});
+	}
+	auto result = std::string();
+	while (x > 0) {
+		result.push_back(static_cast<char>(x % 10 + '0'));
+		x /= 10;
+	}
+	std::reverse(result.begin(), result.end());
+	return result;
+}
+
+// TODO: Make this more efficient
+inline auto to_string(detail::int128_t const x) {
+	return x >= 0 ? to_string(static_cast<detail::uint128_t>(x)) : '-' + to_string(static_cast<detail::uint128_t>(x));
+}
+
+#endif
+
 auto to_string(bounded_integer auto const x) {
 	return to_string(+x.value());
 }
 
-using std::to_wstring;
-
-auto to_wstring(bounded_integer auto const x) {
-	return to_wstring(+x.value());
-}
-
-}	// namespace bounded
+} // namespace bounded
 
