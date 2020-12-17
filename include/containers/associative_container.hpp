0607f6e2b093789b7f0f296ecd9553ab71342264
David Stone <david@doublewise.net>
2020-12-17 10:03:11 -0700

    Added concept `associative_container`

---
+++ include/containers/associative_container.hpp
@@ -0,0 +1,20 @@
+// Copyright David Stone 2020.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <containers/is_range.hpp>
+
+#include <type_traits>
+
+namespace containers {
+
+template<typename T>
+concept associative_container = range<T> and requires {
+	typename std::remove_reference_t<T>::key_type;
+	typename std::remove_reference_t<T>::mapped_type;
+};
+
+} // namespace containers
