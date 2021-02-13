45575852289890f48cc8b6b50b38d7dbdcc93a26
David Stone <david@doublewise.net>
2021-02-13 04:14:02 -0700

    Use test_int for concatenate tests

--- source/containers/algorithms/concatenate.cpp
+++ source/containers/algorithms/concatenate.cpp
@@ -9,57 +9,58 @@
 #include <bounded/copy.hpp>
 
 #include "../../test_assert.hpp"
+#include "../../test_int.hpp"
 
 namespace {
 
 constexpr bool test() {
 	using ::containers::begin;
 	using ::containers::end;
+	using Container = containers::vector<bounded::test_int>;
 
-	auto const a = containers::vector<int>({2, 6, 8});
-	auto const b = containers::vector<int>({3, 5});
-	auto const c = containers::vector<int>({1, 2, 3});
+	auto const a = Container({2, 6, 8});
+	auto const b = Container({3, 5});
+	auto const c = Container({1, 2, 3});
 	
 	auto make_reusable_container = [&]{
-		auto result = containers::vector<int>();
-		result.reserve(static_cast<containers::vector<int>::size_type>(containers::size(a) + containers::size(b) + containers::size(c)));
+		auto result = Container();
+		result.reserve(static_cast<Container::size_type>(containers::size(a) + containers::size(b) + containers::size(c)));
 		return result;
 	};
 	auto const d = make_reusable_container();
 	
-	using Expected = containers::vector<int>;
-	auto const expected_result = Expected({2, 6, 8, 3, 5, 1, 2, 3});
+	auto const expected_result = Container({2, 6, 8, 3, 5, 1, 2, 3});
 	
-	BOUNDED_TEST(::containers::concatenate<Expected>() == Expected());
-	BOUNDED_TEST(::containers::concatenate<Expected>(expected_result) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(expected_result, Expected()) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(Expected(), expected_result) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>() == Container());
+	BOUNDED_TEST(::containers::concatenate<Container>(expected_result) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(expected_result, Container()) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(Container(), expected_result) == expected_result);
 	
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, b, c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, b, c) == expected_result);
 
-	BOUNDED_TEST(::containers::concatenate<Expected>(bounded::copy(a), b, c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, bounded::copy(b), c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, b, bounded::copy(c)) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(bounded::copy(a), b, c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, bounded::copy(b), c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, b, bounded::copy(c)) == expected_result);
 
-	BOUNDED_TEST(::containers::concatenate<Expected>(bounded::copy(a), bounded::copy(b), c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(bounded::copy(a), b, bounded::copy(c)) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, bounded::copy(b), bounded::copy(c)) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(bounded::copy(a), bounded::copy(b), c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(bounded::copy(a), b, bounded::copy(c)) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, bounded::copy(b), bounded::copy(c)) == expected_result);
 
-	BOUNDED_TEST(::containers::concatenate<Expected>(bounded::copy(a), bounded::copy(b), bounded::copy(c)) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(bounded::copy(a), bounded::copy(b), bounded::copy(c)) == expected_result);
 
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, b, c, d) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, b, c, d) == expected_result);
 
-	BOUNDED_TEST(::containers::concatenate<Expected>(bounded::copy(d), a, b, c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, bounded::copy(d), b, c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, b, bounded::copy(d), c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, b, c, bounded::copy(d)) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(bounded::copy(d), a, b, c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, bounded::copy(d), b, c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, b, bounded::copy(d), c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, b, c, bounded::copy(d)) == expected_result);
 
-	BOUNDED_TEST(::containers::concatenate<Expected>(make_reusable_container(), a, b, c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, make_reusable_container(), b, c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, b, make_reusable_container(), c) == expected_result);
-	BOUNDED_TEST(::containers::concatenate<Expected>(a, b, c, make_reusable_container()) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(make_reusable_container(), a, b, c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, make_reusable_container(), b, c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, b, make_reusable_container(), c) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(a, b, c, make_reusable_container()) == expected_result);
 	
-	BOUNDED_TEST(::containers::concatenate<Expected>(make_reusable_container(), a, make_reusable_container(), b, make_reusable_container(), c, make_reusable_container()) == expected_result);
+	BOUNDED_TEST(::containers::concatenate<Container>(make_reusable_container(), a, make_reusable_container(), b, make_reusable_container(), c, make_reusable_container()) == expected_result);
 	
 	BOUNDED_TEST(::containers::concatenate<containers::vector<char>>(std::string_view("a"), std::string_view("b")) == containers::vector<char>({'a', 'b'}));
 	return true;
