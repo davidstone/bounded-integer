e244dddc3eae40b9005c927f9dc6365fd1a17930
David Stone <david@doublewise.net>
2021-02-13 05:23:06 -0700

    `erase` tests uses `test_int`

--- source/containers/erase.cpp
+++ source/containers/erase.cpp
@@ -9,18 +9,18 @@
 #include <containers/vector.hpp>
 
 #include "../test_assert.hpp"
+#include "../test_int.hpp"
 
 namespace {
 
 template<typename Container>
 constexpr auto test_erase() {
 	auto v = Container({1, 2, 3, 4, 5, 6, 7});
-	erase_if(v, [](auto const & value) { return value % 2 == 0; });
+	erase_if(v, [](auto const & x) { return x.value() % 2 == 0; });
 	BOUNDED_TEST(v == Container({1, 3, 5, 7}));
 	return true;
 }
 
-static_assert(test_erase<containers::vector<int>>());
-// static_assert(test_erase<containers::small_buffer_optimized_vector<int, 0>>());
+static_assert(test_erase<containers::vector<bounded::test_int>>());
 
 } // namespace
