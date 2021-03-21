3861c9b8bb14ed4142ddf19f9dbee5045fa2464d
David Stone <david@doublewise.net>
2021-03-21 03:34:33 -0600

    Move `to_radix_sort_key` to its own file

--- include/containers/algorithms/ska_sort.hpp
+++ include/containers/algorithms/ska_sort.hpp
@@ -18,6 +18,7 @@
 #include <containers/is_range.hpp>
 #include <containers/is_iterator_sentinel.hpp>
 #include <containers/size.hpp>
+#include <containers/to_radix_sort_key.hpp>
 
 #include <bounded/detail/copy_cv_ref.hpp>
 #include <operators/forward.hpp>
@@ -34,71 +35,11 @@
 namespace containers {
 namespace detail {
 
-struct unknown_floating_point;
-template<typename T>
-using float_to_unsigned =
-	std::conditional_t<sizeof(T) == 4, std::uint32_t,
-	std::conditional_t<sizeof(T) == 8, std::uint64_t,
-	unknown_floating_point
->>;
-
-} // namespace detail
-
-// TODO: Delete bool overload, currently relying on implicit conversions
-constexpr auto to_radix_sort_key(bool const value) {
-	return value;
-}
-template<typename T> requires std::is_arithmetic_v<T>
-constexpr auto to_radix_sort_key(T const value) {
-	constexpr auto is_character_type =
-		std::is_same_v<T, char> or
-		std::is_same_v<T, char8_t> or
-		std::is_same_v<T, char16_t> or
-		std::is_same_v<T, char32_t> or
-		std::is_same_v<T, wchar_t>;
-
-	if constexpr (std::is_floating_point_v<T>) {
-		static_assert(std::numeric_limits<T>::is_iec559);
-		using unsigned_t = detail::float_to_unsigned<T>;
-		static_assert(sizeof(T) == sizeof(unsigned_t));
-		auto const u = __builtin_bit_cast(unsigned_t, value);
-		constexpr auto sign_bit_position = std::numeric_limits<unsigned_t>::digits - 1U;
-		auto const sign_bit = static_cast<unsigned_t>(-static_cast<std::make_signed_t<unsigned_t>>(u >> (sign_bit_position)));
-		return u ^ (sign_bit | (static_cast<unsigned_t>(1U) << sign_bit_position));
-	} else if constexpr (is_character_type) {
-		return static_cast<std::make_unsigned_t<T>>(value);
-	} else if constexpr (std::is_unsigned_v<T>) {
-		return value;
-	} else {
-		static_assert(std::is_signed_v<T>);
-		using unsigned_t = std::make_unsigned_t<T>;
-		return static_cast<unsigned_t>(static_cast<unsigned_t>(value) + static_cast<unsigned_t>(bounded::min_value<T>));
-	}
-}
-
-constexpr auto to_radix_sort_key(bounded::bounded_integer auto const value) {
-	return to_radix_sort_key(value.value());
-}
-
-template<typename T> requires std::is_enum_v<T>
-constexpr auto to_radix_sort_key(T const value) {
-	return to_radix_sort_key(bounded::integer(value));
-}
-
-auto to_radix_sort_key(auto * ptr) {
-	return reinterpret_cast<std::uintptr_t>(ptr);
-}
-
-namespace detail {
-
 // TODO: Also validate get and tuple_element
 template<typename T>
 concept tuple_like = requires{
 	std::tuple_size<T>::value;
 };
-
-template<typename T>
-concept is_boolean = std::is_arithmetic_v<T> and std::is_same_v<T, bool>;
 
 template<typename T>
 concept indexable =
---
+++ source/containers/to_radix_sort_key.cpp
@@ -0,0 +1,15 @@
+// Copyright David Stone 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <containers/to_radix_sort_key.hpp>
+
+#include "../test_assert.hpp"
+#include "../test_int.hpp"
+
+namespace {
+
+
+
+} // namespace
