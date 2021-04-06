dbc4e726d115b3768580812e9fbc06fa3fea63cc
David Stone <davidfromonline@gmail.com>
2021-04-06 05:15:05 -0600

    Add `offset_type`

---
+++ include/containers/offset_type.hpp
@@ -0,0 +1,33 @@
+// Copyright David Stone 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <bounded/integer.hpp>
+
+#include <iterator>
+
+namespace containers {
+namespace detail {
+
+template<typename Difference>
+struct difference_type_to_offset_type {
+	using type = Difference;
+};
+
+template<bounded::bounded_integer Difference>
+struct difference_type_to_offset_type<Difference> {
+	using type = bounded::integer<
+		0,
+		bounded::normalize<bounded::max_value<Difference>.value()>
+	>;
+};
+
+} // namespace detail
+
+template<typename T>
+using offset_type = typename detail::difference_type_to_offset_type<typename std::iterator_traits<T>::difference_type>::type;
+
+} // namespace containers
