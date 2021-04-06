77334c8a5111cb6d2fce9298828886fbde47e2c1
David Stone <davidfromonline@gmail.com>
2021-04-06 08:36:22 -0600

    Optimize `assign`

--- include/containers/assign.hpp
+++ include/containers/assign.hpp
@@ -5,10 +5,13 @@
 
 #pragma once
 
+#include <containers/algorithms/copy.hpp>
 #include <containers/algorithms/erase.hpp>
 #include <containers/append.hpp>
+#include <containers/assign_to_empty.hpp>
 #include <containers/begin_end.hpp>
 #include <containers/c_array.hpp>
+#include <containers/clear.hpp>
 #include <containers/is_range.hpp>
 #include <containers/range_value_t.hpp>
 #include <containers/range_view.hpp>
@@ -18,19 +21,20 @@
 
 namespace containers {
 
-constexpr void assign(resizable_container auto & destination, range auto && source) {
-	auto it = containers::begin(destination);
-	auto first = containers::begin(OPERATORS_FORWARD(source));
-	auto last = containers::end(OPERATORS_FORWARD(source));
-	for (; first != last; ++first) {
-		if (it == containers::end(destination)) {
-			break;
-		}
-		*it = *first;
-		++it;
+template<resizable_container Target>
+constexpr void assign(Target & target, range auto && source) {
+	// Either of these implementations work for any type. For types that are not
+	// trivially copyable, we want to reuse the existing storage where that
+	// makes sense. For types that are trivially copyable, that concept doesn't
+	// make sense, so we instead use code that optimizes better.
+	if constexpr (std::is_trivially_copyable_v<range_value_t<Target>>) {
+		::containers::clear(target);
+		::containers::assign_to_empty(target, OPERATORS_FORWARD(source));
+	} else {
+		auto result = ::containers::copy(OPERATORS_FORWARD(source), OPERATORS_FORWARD(target));
+		::containers::erase_after(target, std::move(result.output));
+		::containers::append(target, range_view(std::move(result.input), containers::end(OPERATORS_FORWARD(source))));
 	}
-	::containers::erase(destination, it, containers::end(destination));
-	::containers::append(destination, range_view(first, last));
 }
 template<resizable_container Container, std::size_t init_size>
 constexpr void assign(Container & destination, c_array<range_value_t<Container>, init_size> && init) {
