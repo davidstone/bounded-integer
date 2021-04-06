a2ba18065d9218ab586b5125eb8f1cdeacbda5b8
David Stone <davidfromonline@gmail.com>
2021-04-05 18:11:39 -0600

    Optimize `clear`

--- include/containers/clear.hpp
+++ include/containers/clear.hpp
@@ -5,21 +5,29 @@
 
 #pragma once
 
+#include <containers/algorithms/destroy_range.hpp>
 #include <containers/is_empty.hpp>
 #include <containers/pop_back.hpp>
 #include <containers/pop_front.hpp>
+#include <containers/size.hpp>
 
 #include <bounded/integer.hpp>
 
 namespace containers {
 
 template<typename Container> requires detail::pop_backable<Container> or detail::pop_frontable<Container>
-constexpr auto clear(Container & value) {
-	while (!containers::is_empty(value)) {
-		if constexpr (detail::pop_backable<Container>) {
-			containers::pop_back(value);
-		} else {
-			containers::pop_front(value);
+constexpr auto clear(Container & container) {
+	// TODO: Link to Godbolt showing better code gen
+	if constexpr (detail::has_member_append_from_capacity<Container>) {
+		containers::destroy_range(container);
+		container.append_from_capacity(-containers::size(container));
+	} else {
+		while (!containers::is_empty(container)) {
+			if constexpr (detail::pop_backable<Container>) {
+				containers::pop_back(container);
+			} else {
+				containers::pop_front(container);
+			}
 		}
 	}
 }
