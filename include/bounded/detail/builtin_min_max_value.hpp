50910ea3908153c5375ce12cc82713462d19f15f
David Stone <davidfromonline@gmail.com>
2021-04-17 09:09:52 -0600

    Move `as_builtin_integer` into `normalize.hpp` and handle enums in it.

--- include/bounded/detail/builtin_min_max_value.hpp
+++ include/bounded/detail/builtin_min_max_value.hpp
@@ -12,14 +12,6 @@
 namespace bounded {
 namespace detail {
 
-constexpr auto as_builtin_integer(auto const value) {
-	if constexpr (is_bounded_integer<decltype(value)>) {
-		return value.value();
-	} else {
-		return value;
-	}
-}
-
 template<typename T> requires(!std::is_same_v<decltype(max_value<T>), incomplete>)
 inline constexpr auto builtin_max_value = normalize<as_builtin_integer(max_value<T>)>;
 
--- include/bounded/normalize.hpp
+++ include/bounded/normalize.hpp
@@ -7,11 +7,25 @@
 
 #include <bounded/detail/max_builtin.hpp>
 #include <bounded/detail/min_max_value.hpp>
+#include <bounded/detail/is_bounded_integer.hpp>
 
 #include <type_traits>
+#include <utility>
 
 namespace bounded {
 namespace detail {
+
+template<typename T>
+constexpr auto as_builtin_integer(T const x) {
+	if constexpr (is_integral_constant_of_integral<T>) {
+		return x.get();
+	} else if constexpr (std::is_enum_v<T>) {
+		return std::to_underlying(x);
+	} else {
+		static_assert(builtin_integer<T> or std::is_same_v<T, bool>);
+		return x;
+	}
+}
 
 constexpr auto must_use_unsigned(auto const value) {
 	return value > 0 and static_cast<max_unsigned_t>(value) > static_cast<max_unsigned_t>(max_value<max_signed_t>);
@@ -28,12 +42,9 @@ constexpr auto fits_in_int(T const value) {
 
 template<auto value>
 inline constexpr auto normalize = static_cast<
-	std::conditional_t<detail::fits_in_int(+value), int,
-	std::conditional_t<detail::must_use_unsigned(value), detail::max_unsigned_t,
+	std::conditional_t<detail::fits_in_int(+detail::as_builtin_integer(value)), int,
+	std::conditional_t<detail::must_use_unsigned(detail::as_builtin_integer(value)), detail::max_unsigned_t,
 	detail::max_signed_t
 >>>(value);
-
-template<auto value> requires std::is_enum_v<decltype(value)>
-inline constexpr auto normalize<value> = normalize<static_cast<std::underlying_type_t<decltype(value)>>(value)>;
 
 } // namespace bounded
