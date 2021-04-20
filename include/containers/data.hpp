9de7a25847130595e16e46231dceae1c8ac5a81b
David Stone <davidfromonline@gmail.com>
2021-04-20 05:18:58 -0600

    Constrain `containers::data`

--- include/containers/data.hpp
+++ include/containers/data.hpp
@@ -6,16 +6,31 @@
 #pragma once
 
 #include <containers/begin_end.hpp>
-#include <containers/common_iterator_functions.hpp>
 #include <containers/is_range.hpp>
+#include <containers/iterator_t.hpp>
 #include <containers/to_address.hpp>
 
 #include <bounded/integer.hpp>
 
 namespace containers {
+namespace detail {
 
-constexpr auto data(range auto && r) {
-	if constexpr (requires { r.data(); }) {
+template<typename Range>
+concept has_member_data = requires(Range r) { r.data(); };
+
+template<typename Iterator, typename Sentinel>
+concept contiguous_iterator_sentinel = to_addressable<Iterator> and random_access_sentinel_for<Sentinel, Iterator>;
+
+} // namespace detail
+
+template<typename T>
+concept contiguous_range =
+	random_access_range<T> and
+	(detail::has_member_data<T> or detail::contiguous_iterator_sentinel<iterator_t<T>, detail::sentinel_t<T>>);
+
+template<contiguous_range Range>
+constexpr auto data(Range && r) {
+	if constexpr (detail::has_member_data<Range>) {
 		return r.data();
 	} else {
 		return containers::to_address(containers::begin(r));
