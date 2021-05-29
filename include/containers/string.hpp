58e9ce903c8bf84cee21e391acd00bcd1ac29865
David Stone <davidfromonline@gmail.com>
2021-05-29 05:55:43 -0600

    `containers::string` is no longer an alias. Removed `basic_string`

--- include/containers/string.hpp
+++ include/containers/string.hpp
@@ -19,40 +19,31 @@
 #include <type_traits>
 
 namespace containers {
-namespace detail {
 
-template<typename CharT>
-concept char_like = std::is_trivial_v<CharT> and std::is_standard_layout_v<CharT> and !std::is_array_v<CharT>;
-
-} // namespace detail
-
-// Unlike std::basic_string, there is no null terminator.
-template<typename CharT>
-struct basic_string : private small_buffer_optimized_vector<CharT, 0> {
-	static_assert(detail::char_like<CharT>);
-private:
-	using base = small_buffer_optimized_vector<CharT, 0>;
+// Unlike std::string, there is no null terminator.
+struct string : private small_buffer_optimized_vector<char, 0> {
+	using base = small_buffer_optimized_vector<char, 0>;
 public:
-	using value_type = CharT;
-	using typename base::size_type;
-	using typename base::const_iterator;
-	using typename base::iterator;
+	using value_type = char;
+	using base::size_type;
+	using base::const_iterator;
+	using base::iterator;
 	
 	using base::base;
 	
-	constexpr basic_string(std::basic_string_view<CharT> const sv):
+	constexpr string(std::string_view const sv):
 		base(sv)
 	{
 	}
-	constexpr basic_string(CharT const * const c_str):
-		base(std::basic_string_view<CharT>(c_str))
+	constexpr string(char const * const c_str):
+		base(std::string_view(c_str))
 	{
 	}
 
-	basic_string(basic_string const &) = default;
-	basic_string(basic_string &&) = default;
-	basic_string & operator=(basic_string const &) & = default;
-	basic_string & operator=(basic_string &&) & = default;
+	string(string const &) = default;
+	string(string &&) = default;
+	string & operator=(string const &) & = default;
+	string & operator=(string &&) & = default;
 	using base::operator=;
 	
 	using base::begin;
@@ -65,72 +56,44 @@ public:
 	
 	using base::append_from_capacity;
 	
-	constexpr operator std::basic_string_view<CharT>() const {
-		return {data(*this), static_cast<typename std::basic_string_view<CharT>::size_type>(size())};
+	constexpr operator std::string_view() const {
+		return std::string_view(data(*this), static_cast<std::string_view::size_type>(size()));
+	}
+	friend constexpr auto operator<=>(string const & lhs, std::string_view const rhs) {
+		return ::containers::lexicographical_compare_3way(lhs, rhs);
+	}
+	friend constexpr auto operator==(string const & lhs, std::string_view const rhs) -> bool {
+		return ::containers::equal(lhs, rhs);
+	}
+
+	friend auto & operator<<(std::ostream & stream, string const & str) {
+		return stream << std::string_view(str);
+	}
+
+	friend auto & operator>>(std::istream & stream, string & str) {
+		auto const sentry = std::istream::sentry(stream);
+		if (!sentry) {
+			return stream;
+		}
+		constexpr auto max_width = numeric_traits::max_value<string::size_type>;
+		auto const width = stream.width();
+		auto const max_characters = width <= 0 ? max_width : bounded::clamp(bounded::integer(width), 1_bi, max_width);
+		containers::clear(str);
+		for (auto it = std::istreambuf_iterator(stream); it != std::istreambuf_iterator<char>(); ++it) {
+			if (containers::size(str) == max_characters) {
+				break;
+			}
+			if (std::isspace(*it, stream.getloc())) {
+				break;
+			}
+			containers::push_back(str, *it);
+		}
+		if (containers::is_empty(str)) {
+			stream.setstate(std::ios::failbit);
+		}
+		stream.width(0);
+		return stream;
 	}
 };
 
-
-template<typename CharT>
-constexpr auto operator<=>(basic_string<CharT> const & lhs, std::basic_string_view<std::type_identity_t<CharT>> const rhs) {
-	return ::containers::lexicographical_compare_3way(lhs, rhs);
-}
-
-template<typename CharT>
-constexpr auto operator==(basic_string<CharT> const & lhs, std::basic_string_view<std::type_identity_t<CharT>> const rhs) -> bool {
-	return ::containers::equal(lhs, rhs);
-}
-
-
-namespace detail {
-
-template<typename T>
-inline constexpr auto is_string = false;
-
-template<typename CharT>
-inline constexpr auto is_string<basic_string<CharT>> = true;
-
-template<typename T>
-concept string_specialization = is_string<std::decay_t<T>>;
-
-}	// namespace detail
-
-using string = basic_string<char>;
-using wstring = basic_string<wchar_t>;
-using u16string = basic_string<char16_t>;
-using u32string = basic_string<char32_t>;
-
-template<typename CharT>
-auto & operator<<(std::basic_ostream<CharT> & stream, basic_string<CharT> const & str) {
-	return stream << static_cast<std::string_view>(str);
-}
-
-template<typename CharT>
-auto & operator>>(std::basic_istream<CharT> & stream, basic_string<CharT> & str) {
-	auto const sentry = typename std::basic_istream<CharT>::sentry(stream);
-	if (!sentry) {
-		return stream;
-	}
-	constexpr auto max_width = numeric_traits::max_value<typename basic_string<CharT>::size_type>;
-	auto const width = stream.width();
-	auto const max_characters = width <= 0 ? max_width : bounded::clamp(bounded::integer(width), 1_bi, max_width);
-	containers::clear(str);
-	for (auto it = std::istreambuf_iterator(stream); it != std::istreambuf_iterator<CharT>(); ++it) {
-		if (containers::size(str) == max_characters) {
-			break;
-		}
-		if (std::isspace(*it, stream.getloc())) {
-			break;
-		}
-		containers::push_back(str, *it);
-	}
-	if (containers::is_empty(str)) {
-		stream.setstate(std::ios::failbit);
-	}
-	stream.width(0);
-	return stream;
-}
-
-// TODO: Add in a null-terminated string class
-
-}	// namespace containers
+} // namespace containers
