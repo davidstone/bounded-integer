076b3acd725fac02c10d9a5de08997cacedcca0b
David Stone <davidfromonline@gmail.com>
2021-05-30 03:06:20 -0600

    Add more `size` unit tests

--- source/containers/size.cpp
+++ source/containers/size.cpp
@@ -10,7 +10,39 @@ namespace {
 using namespace bounded::literal;
 
 constexpr int a[] = {0, 1, 2, 3, 4};
-
 static_assert(containers::size(a) == 5_bi);
+
+struct static_member_size {
+	static constexpr auto begin() -> int const * {
+		return nullptr;
+	}
+	static constexpr auto size() {
+		return 1;
+	}
+};
+void f(static_member_size parameter) {
+	static_assert(containers::size(parameter) == 1);
+}
+
+struct member_size {
+	constexpr auto begin() const -> int const * {
+		return nullptr;
+	}
+	constexpr auto size() const {
+		return 2;
+	}
+};
+static_assert(containers::size(member_size()) == 2);
+
+struct member_end {
+	using size_type = int;
+	constexpr auto begin() const -> int const * {
+		return a;
+	}
+	constexpr auto end() const -> int const * {
+		return begin() + 3;
+	}
+};
+static_assert(containers::size(member_end()) == 3);
 
 } // namespace
