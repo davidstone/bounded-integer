7f442095466db08ff31be26829800c33fa576e4b
David Stone <davidfromonline@gmail.com>
2021-06-01 11:38:53 -0600

    Add `iterator_t` unit tests

--- source/containers/iterator_t.cpp
+++ source/containers/iterator_t.cpp
@@ -4,3 +4,22 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/iterator_t.hpp>
+
+#include <containers/array/array.hpp>
+
+namespace {
+
+static_assert(
+	std::is_same_v<containers::iterator_t<containers::array<int, 3> const &>,
+	containers::contiguous_iterator<int const, 3>>
+);
+static_assert(std::is_same_v<
+	containers::iterator_t<containers::array<int, 3> &>,
+	containers::contiguous_iterator<int, 3>
+>);
+static_assert(std::is_same_v<
+	containers::iterator_t<containers::array<int, 3> &&>,
+	decltype(containers::move_iterator(std::declval<containers::contiguous_iterator<int, 3>>()))
+>);
+
+} // namespace
