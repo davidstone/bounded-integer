27dbaa74f870d3b5aed2496a1ac01b437014ad3f
David Stone <davidfromonline@gmail.com>
2021-06-12 05:54:17 -0600

    Simplify implementation of division. Improve unit tests. Improve compile time performance.

--- include/bounded/detail/arithmetic/divides.hpp
+++ include/bounded/detail/arithmetic/divides.hpp
@@ -7,7 +7,6 @@
 
 #include <bounded/detail/arithmetic/base.hpp>
 #include <bounded/detail/arithmetic/safe_abs.hpp>
-#include <bounded/detail/arithmetic/unary_minus.hpp>
 #include <bounded/detail/comparison.hpp>
 #include <bounded/detail/max_builtin.hpp>
 #include <bounded/detail/minmax.hpp>
@@ -30,35 +29,40 @@ constexpr auto conditional_function(auto if_true, auto if_false) {
 template<auto lhs, auto rhs>
 constexpr auto safer_divide() {
 	constexpr auto is_negative = (lhs < 0) xor (rhs < 0);
-	constexpr auto positive_result = bounded::constant<safe_abs(lhs) / safe_abs(rhs)>;
+	constexpr auto positive_result = safe_abs(lhs) / safe_abs(rhs);
 	if constexpr (is_negative) {
-		return (-positive_result).value();
+		return static_cast<max_signed_t>(-positive_result);
 	} else {
-		return positive_result.value();
+		return positive_result;
 	}
 }
 
-constexpr auto divides_operator_range = [](auto const lhs_, auto const rhs_) {
-	constexpr auto lhs = min_max{lhs_.min, lhs_.max};
-	constexpr auto rhs = min_max{rhs_.min, rhs_.max};
-	// If 1 falls within the range, that is the least positive divisor. The
-	// other options are a range that are entirely positive, in which case I
-	// want to return the least value, or the range is entirely negative or
-	// zero, in which case I pick the greatest absolute value (which is the
-	// minimum) so that the 'positive' divisor is not selected in a later step.
-	// We can use similar logic for greatest_negative_divisor.
-	constexpr auto least_positive_divisor = ::bounded::detail::conditional_function<rhs.min <= constant<1> and constant<1> <= rhs.max>(1, rhs.min.value());
-	constexpr auto greatest_negative_divisor = ::bounded::detail::conditional_function<rhs.min <= constant<-1> and constant<-1> <= rhs.max>(-1, rhs.max.value());
+constexpr auto positive_divisor_division(auto const lhs, auto const rhs) {
+	constexpr auto minimum_divisor = conditional_function<lhs.min.value() < 0>(rhs.min, rhs.max).value();
+	constexpr auto maximum_divisor = conditional_function<lhs.max.value() < 0>(rhs.max, rhs.min).value();
+	return min_max{safer_divide<lhs.min.value(), minimum_divisor>(), safer_divide<lhs.max.value(), maximum_divisor>()};
+}
+constexpr auto negative_divisor_division(auto const lhs, auto const rhs) {
+	constexpr auto minimum_divisor = conditional_function<lhs.max.value() >= 0>(rhs.max, rhs.min).value();
+	constexpr auto maximum_divisor = conditional_function<lhs.min.value() >= 0>(rhs.min, rhs.max).value();
+	return min_max{safer_divide<lhs.max.value(), minimum_divisor>(), safer_divide<lhs.min.value(), maximum_divisor>()};
+}
 
-	constexpr auto g0 = safer_divide<lhs.min.value(), least_positive_divisor>();
-	constexpr auto g1 = safer_divide<lhs.min.value(), greatest_negative_divisor>();
-	constexpr auto g2 = safer_divide<lhs.max.value(), least_positive_divisor>();
-	constexpr auto g3 = safer_divide<lhs.max.value(), greatest_negative_divisor>();
-
-	return min_max{
-		safe_min(g0, g1, g2, g3),
-		safe_max(g0, g1, g2, g3)
-	};
+constexpr auto divides_operator_range = [](auto const lhs, auto const rhs) {
+	static_assert(rhs.min != constant<0>);
+	static_assert(rhs.max != constant<0>);
+	if constexpr (rhs.min >= constant<1>) {
+		return positive_divisor_division(lhs, rhs);
+	} else if constexpr (rhs.max <= constant<-1>) {
+		return negative_divisor_division(lhs, rhs);
+	} else {
+		constexpr auto positive = positive_divisor_division(lhs, min_max{constant<1>, rhs.max});
+		constexpr auto negative = negative_divisor_division(lhs, min_max{rhs.min, constant<-1>});
+		return min_max{
+			safe_min(positive.min, negative.min),
+			safe_max(positive.max, negative.max)
+		};
+	}
 };
 
 // #include <functional> is really expensive, and we just need this one part
--- source/bounded/detail/arithmetic/divides.cpp
+++ source/bounded/detail/arithmetic/divides.cpp
@@ -4,6 +4,7 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <bounded/detail/arithmetic/divides.hpp>
+#include <bounded/detail/arithmetic/unary_minus.hpp>
 #include <bounded/detail/class.hpp>
 #include "extreme_values.hpp"
 #include "../../homogeneous_equals.hpp"
@@ -71,8 +72,38 @@ static_assert(homogeneous_equals(
 ));
 
 static_assert(homogeneous_equals(
+	bounded::integer<7, 100>(bounded::constant<9>) / bounded::integer<3, 5>(bounded::constant<4>),
+	bounded::integer<1, 33>(bounded::constant<2>)
+));
+
+static_assert(homogeneous_equals(
+	bounded::integer<-76, -14>(bounded::constant<-20>) / bounded::integer<4, 20>(bounded::constant<4>),
+	bounded::integer<-19, 0>(bounded::constant<-5>)
+));
+
+static_assert(homogeneous_equals(
+	bounded::integer<7, 100>(bounded::constant<9>) / bounded::integer<-8, -2>(bounded::constant<-4>),
+	bounded::integer<-50, 0>(bounded::constant<-2>)
+));
+
+static_assert(homogeneous_equals(
+	bounded::integer<-76, -14>(bounded::constant<-20>) / bounded::integer<-21, -6>(bounded::constant<-10>),
+	bounded::integer<0, 12>(bounded::constant<2>)
+));
+
+static_assert(homogeneous_equals(
 	bounded::integer<1, 10>(bounded::constant<9>) / bounded::integer<-3, 11>(bounded::constant<4>),
 	bounded::integer<-10, 10>(bounded::constant<2>)
+));
+
+static_assert(homogeneous_equals(
+	bounded::integer<-71, 6>(bounded::constant<-12>) / bounded::integer<5, 12>(bounded::constant<6>),
+	bounded::integer<-14, 1>(bounded::constant<-2>)
+));
+
+static_assert(homogeneous_equals(
+	bounded::integer<-20, 30>(bounded::constant<9>) / bounded::integer<-7, 3>(bounded::constant<2>),
+	bounded::integer<-30, 30>(bounded::constant<4>)
 ));
 
 // constexpr auto fails_to_compile = bounded::constant<1> / bounded::constant<0>;
