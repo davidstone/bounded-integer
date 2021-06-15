4f337a554a8b5545b1551e16e39e0a397d97d5f6
David Stone <davidfromonline@gmail.com>
2021-06-15 08:25:53 -0600

    Improve compile times of plus

--- include/bounded/detail/arithmetic/plus.hpp
+++ include/bounded/detail/arithmetic/plus.hpp
@@ -15,24 +15,20 @@ namespace bounded {
 namespace detail {
 
 template<auto lhs, auto rhs>
-constexpr auto safer_add(constant_t<lhs>, constant_t<rhs>) {
+constexpr auto safer_add() {
 	constexpr auto modulo_equivalent_value = static_cast<max_unsigned_t>(lhs) + static_cast<max_unsigned_t>(rhs);
-	constexpr auto max_signed = numeric_traits::max_value<max_signed_t>;
-	if constexpr ((lhs >= 0 and rhs >= 0) or lhs > max_signed or rhs > max_signed) {
+	if constexpr (lhs >= 0 and rhs >= 0) {
 		static_assert(
-			(lhs < 0 or rhs < 0) or
-			(modulo_equivalent_value >= static_cast<max_unsigned_t>(lhs) and modulo_equivalent_value >= static_cast<max_unsigned_t>(rhs)),
+			modulo_equivalent_value >= static_cast<max_unsigned_t>(lhs) and modulo_equivalent_value >= static_cast<max_unsigned_t>(rhs),
 			"Overflow in calculation of bounds."
 		);
 		return modulo_equivalent_value;
+	} else if constexpr (lhs > max_signed or rhs > max_signed) {
+		return modulo_equivalent_value;
 	} else {
-		// To enter this case, we know that at least one of lhs and rhs is
-		// negative. If at least one of them is positive, addition cannot
-		// underflow. If both are negative, we rely on modular arithmetic to
-		// detect overflow.
 		static_assert(
 			lhs > 0 or rhs > 0 or modulo_equivalent_value > max_signed,
-			"Underflow in calculation of bounds."
+			"Overflow in calculation of bounds."
 		);
 		return static_cast<max_signed_t>(modulo_equivalent_value);
 	}
@@ -48,8 +44,8 @@ inline constexpr auto plus = [](auto const lhs, auto const rhs) {
 constexpr auto operator+(bounded_integer auto const lhs_, bounded_integer auto const rhs_) {
 	return detail::modulo_equivalent_operator_overload(lhs_, rhs_, detail::plus, [](auto const lhs, auto const rhs) {
 		return detail::min_max{
-			::bounded::detail::safer_add(lhs.min, rhs.min),
-			::bounded::detail::safer_add(lhs.max, rhs.max)
+			::bounded::detail::safer_add<lhs.min.value(), rhs.min.value()>(),
+			::bounded::detail::safer_add<lhs.max.value(), rhs.max.value()>()
 		};
 	});
 }
