21dee81fe8198a0a8ae5307359d90c56d3936057
David Stone <davidfromonline@gmail.com>
2021-07-02 16:11:52 -0600

    Make `make_static_vector` useful

--- include/containers/static_vector/make_static_vector.hpp
+++ include/containers/static_vector/make_static_vector.hpp
@@ -1,4 +1,4 @@
-// Copyright David Stone 2015.
+// Copyright David Stone 2021.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -7,15 +7,19 @@
 
 #include <containers/static_vector/static_vector.hpp>
 
-#include <numeric_traits/min_max_value.hpp>
+#include <containers/is_range.hpp>
+#include <containers/range_value_t.hpp>
+#include <containers/size.hpp>
 
-#include <limits>
+#include <numeric_traits/min_max_value.hpp>
 
 namespace containers {
 
-template<typename T, typename Size>
-constexpr auto make_static_vector(Size const size) {
-	return static_vector<T, static_cast<std::size_t>(numeric_traits::max_value<Size>)>(size);
+template<range Source>
+constexpr auto make_static_vector(Source && source) {
+	using value_type = range_value_t<Source>;
+	constexpr auto size = static_cast<std::size_t>(numeric_traits::max_value<range_size_t<Source>>);
+	return static_vector<value_type, size>(OPERATORS_FORWARD(source));
 }
 
 }	// namespace containers
--- source/containers/static_vector/make_static_vector.cpp
+++ source/containers/static_vector/make_static_vector.cpp
@@ -1,6 +1,19 @@
-// Copyright David Stone 2015.
+// Copyright David Stone 2021.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/static_vector/make_static_vector.hpp>
+
+#include <containers/array/array.hpp>
+
+#include "../../bounded/homogeneous_equals.hpp"
+
+namespace {
+
+static_assert(homogeneous_equals(
+	containers::make_static_vector(containers::array({5, 3})),
+	containers::static_vector<int, 2>({5, 3})
+));
+
+} // namespace
