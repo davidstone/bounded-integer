6c02f97f2a70a9da4cdec4c7c69b0c55e6cd4fca
David Stone <davidfromonline@gmail.com>
2021-08-08 06:52:26 -0600

    Add `assign` unit test

--- source/containers/assign.cpp
+++ source/containers/assign.cpp
@@ -4,3 +4,22 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/assign.hpp>
+
+#include <containers/algorithms/compare.hpp>
+#include <containers/vector.hpp>
+
+#include "../test_int.hpp"
+
+namespace {
+
+template<typename Container>
+constexpr auto test_assign(auto const & source) -> bool {
+	auto container = Container();
+	containers::assign(container, source);
+	BOUNDED_ASSERT(containers::equal(container, source));
+	return true;
+}
+
+static_assert(test_assign<containers::vector<bounded_test::integer>>(containers::vector({bounded_test::integer(2)})));
+
+} // namespace
