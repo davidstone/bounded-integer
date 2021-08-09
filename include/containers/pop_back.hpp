cb5fa1ed994a2a9375797603920eccafe18b01f2
David Stone <davidfromonline@gmail.com>
2021-08-09 04:54:34 -0600

    Expand `pop_back` and `pop_front` to take advantage of `constant_time_erasable`

--- include/containers/pop_back.hpp
+++ include/containers/pop_back.hpp
@@ -5,7 +5,11 @@
 
 #pragma once
 
+#include <containers/algorithms/advance.hpp>
+#include <containers/algorithms/erase.hpp>
+#include <containers/begin_end.hpp>
 #include <containers/front_back.hpp>
+#include <containers/constant_time_erasable.hpp>
 #include <containers/is_range.hpp>
 
 #include <bounded/detail/construct_destroy.hpp>
@@ -21,13 +25,16 @@ template<typename Container>
 concept has_member_append_from_capacity = requires (Container & container) { container.append_from_capacity(bounded::constant<-1>); };
 
 template<typename Container>
-concept pop_backable = has_member_pop_back<Container> or has_member_append_from_capacity<Container>;
+concept pop_backable = has_member_pop_back<Container> or has_member_append_from_capacity<Container> or constant_time_erasable<Container>;
 
 } // namespace detail
 
-constexpr auto pop_back(detail::pop_backable auto & c) -> void {
-	if constexpr (detail::has_member_pop_back<decltype(c)>) {
+template<detail::pop_backable Container>
+constexpr auto pop_back(Container & c) -> void {
+	if constexpr (detail::has_member_pop_back<Container>) {
 		c.pop_back();
+	} else if constexpr (constant_time_erasable<Container>) {
+		containers::erase(c, containers::prev(containers::end(c)));
 	} else {
 		bounded::destroy(containers::back(c));
 		c.append_from_capacity(bounded::constant<-1>);
--- include/containers/pop_front.hpp
+++ include/containers/pop_front.hpp
@@ -5,7 +5,10 @@
 
 #pragma once
 
+#include <containers/algorithms/erase.hpp>
+#include <containers/begin_end.hpp>
 #include <containers/front_back.hpp>
+#include <containers/constant_time_erasable.hpp>
 #include <containers/is_range.hpp>
 
 #include <bounded/detail/construct_destroy.hpp>
@@ -15,12 +18,21 @@ namespace containers {
 namespace detail {
 
 template<typename Container>
-concept pop_frontable = requires (Container & container) { container.pop_front(); };
+concept member_pop_frontable = requires (Container & container) { container.pop_front(); };
+
+template<typename Container>
+concept pop_frontable = member_pop_frontable<Container> or constant_time_erasable<Container>;
 
 } // namespace detail
 
-constexpr auto pop_front(detail::pop_frontable auto & c) -> void {
-	c.pop_front();
+template<detail::pop_frontable Container>
+constexpr auto pop_front(Container & c) -> void {
+	if constexpr (detail::member_pop_frontable<Container>) {
+		c.pop_front();
+	} else {
+		static_assert(constant_time_erasable<Container>);
+		containers::erase(c, containers::begin(c));
+	}
 }
 
-}	// namespace containers
+} // namespace containers
