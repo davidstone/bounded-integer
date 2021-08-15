ee2353be78a03ab19a97d9b02a832d3c6404469c
David Stone <davidfromonline@gmail.com>
2021-08-15 13:02:01 -0600

    Add tests for `containers::sum`

--- source/containers/algorithms/accumulate.cpp
+++ source/containers/algorithms/accumulate.cpp
@@ -4,3 +4,14 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/algorithms/accumulate.hpp>
+
+#include <containers/array/array.hpp>
+
+namespace {
+
+using namespace bounded::literal;
+
+static_assert(containers::sum(containers::array{0_bi, 3_bi, 2_bi, 3_bi, 5_bi}) == (0_bi + 3_bi + 2_bi + 3_bi + 5_bi));
+static_assert(containers::sum(containers::array({0.0, 1.0, 2.0})) == (0.0 + 1.0 + 2.0));
+
+} // namespace
