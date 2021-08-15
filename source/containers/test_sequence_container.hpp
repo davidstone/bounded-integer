0da629acb672d9725e2b395da8fc3b33b4475238
David Stone <davidfromonline@gmail.com>
2021-08-15 12:51:48 -0600

    Removed redundant test

--- source/containers/test_sequence_container.hpp
+++ source/containers/test_sequence_container.hpp
@@ -189,7 +189,6 @@ constexpr auto test_sequence_container() -> bool {
 	test_sequence_container_single<Container>({});
 	test_sequence_container_single<Container>({5});
 	test_sequence_container_single<Container>({0, 1, 4});
-	test_sequence_container_single<Container>({0, 1, 2, 3, 4});
 	test_sequence_container_single<Container>({
 		0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 		0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
