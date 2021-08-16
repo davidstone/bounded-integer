aae4b9ed3de443ef87edb1ff66ce9b327b72478c
David Stone <davidfromonline@gmail.com>
2021-08-16 15:01:20 -0600

    Add test that `dynamic_array` is convertible to `span`

--- source/containers/dynamic_array.cpp
+++ source/containers/dynamic_array.cpp
@@ -13,6 +13,9 @@ namespace {
 static_assert(containers_test::test_sequence_container<containers::dynamic_array<int>>());
 static_assert(containers_test::test_sequence_container<containers::dynamic_array<bounded_test::integer>>());
 
+static_assert(std::is_convertible_v<containers::dynamic_array<bounded_test::integer> const &, std::span<bounded_test::integer const>>);
+static_assert(std::is_convertible_v<containers::dynamic_array<bounded_test::integer> &, std::span<bounded_test::integer>>);
+
 struct recursive {
 	containers::dynamic_array<recursive, bounded::integer<0, 1>> m;
 };
