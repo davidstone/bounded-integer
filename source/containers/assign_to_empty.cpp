78f3acef558b6ba04f8bfc743fbd7a2887a8e500
David Stone <davidfromonline@gmail.com>
2021-10-09 05:43:03 -0600

    Add `assign_to_empty` unit tests

--- source/containers/assign_to_empty.cpp
+++ source/containers/assign_to_empty.cpp
@@ -4,3 +4,53 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/assign_to_empty.hpp>
+
+#include <containers/vector.hpp>
+
+#include "../test_assert.hpp"
+#include "../test_int.hpp"
+
+namespace {
+
+using namespace bounded::literal;
+
+using non_copyable = bounded_test::non_copyable_integer;
+
+using container = containers::vector<non_copyable>;
+
+constexpr auto test_assign(bool const same_allocation, auto make_initial, auto... additional) {
+	auto individual_test = [&](auto do_assign) {
+		auto c = make_initial();
+		BOUNDED_TEST(containers::is_empty(c));
+		auto const ptr = containers::data(c);
+		do_assign(c);
+		BOUNDED_TEST(c == container({copy_value(additional)...}));
+		if (same_allocation) {
+			BOUNDED_TEST(ptr == containers::data(c));
+		}
+	};
+	individual_test([&](container & c) { containers::assign_to_empty(c, {copy_value(additional)...}); });
+	individual_test([&](container & c) { containers::assign_to_empty(c, container({copy_value(additional)...})); });
+	return true;
+}
+
+static_assert(test_assign(
+	true,
+	[] { return container(); }
+));
+static_assert(test_assign(
+	false,
+	[] { return container(); },
+	non_copyable(2)
+));
+static_assert(test_assign(
+	true,
+	[] {
+		auto c = container();
+		c.reserve(2_bi);
+		return c;
+	},
+	non_copyable(2)
+));
+
+} // namespace
