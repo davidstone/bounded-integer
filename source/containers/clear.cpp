80fa9bc331d385a0dbfa74ebcb309fcc361bab77
David Stone <davidfromonline@gmail.com>
2021-10-10 03:59:01 -0600

    Add `clear` unit tests

--- source/containers/clear.cpp
+++ source/containers/clear.cpp
@@ -4,3 +4,27 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/clear.hpp>
+
+#include <containers/is_empty.hpp>
+#include <containers/vector.hpp>
+
+#include "../test_assert.hpp"
+#include "../test_int.hpp"
+
+namespace {
+
+static_assert([]{
+	auto v = containers::vector<bounded_test::non_copyable_integer>({1});
+	containers::clear(v);
+	BOUNDED_TEST(containers::is_empty(v));
+	return true;
+}());
+
+static_assert([]{
+	auto v = containers::vector<bounded_test::non_copyable_integer>();
+	containers::clear(v);
+	BOUNDED_TEST(containers::is_empty(v));
+	return true;
+}());
+
+} // namespace
