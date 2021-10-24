aaf6b143f26d7a1158c78fab9c7228aa72870995
David Stone <davidfromonline@gmail.com>
2021-10-24 04:38:13 -0600

    Add more vector benchmarks

--- source/containers/std/vector_benchmark.cpp
+++ source/containers/std/vector_benchmark.cpp
@@ -7,15 +7,71 @@
 
 #include <containers/std/vector.hpp>
 
-#include <numeric_traits/min_max_value.hpp>
-
-#include <iostream>
+#include <cstddef>
+#include <memory>
+#include <type_traits>
 #include <vector>
 
 namespace {
 
+template<typename T>
+auto default_value() {
+	if constexpr (std::is_integral_v<T>) {
+		return 0;
+	} else {
+		return T(1024, typename T::value_type());
+	}
+}
+
+template <class Container>
+void benchmark_construct_size(benchmark::State & state) {
+  auto const size = state.range(0);
+  for (auto _ : state) {
+	auto c = Container(size);
+	benchmark::DoNotOptimize(c.data());
+  }
+}
+
+template <class Container, typename Container::value_type value>
+void benchmark_construct_repeat_value(benchmark::State & state) {
+  auto const size = state.range(0);
+  for (auto _ : state) {
+	auto c = Container(size, value);
+	benchmark::DoNotOptimize(c.data());
+  }
+}
+
+template <class Container>
+void benchmark_construct_iterator_pair(benchmark::State & state) {
+	auto in = std::vector(state.range(0), default_value<typename Container::value_type>());
+	auto const begin = in.begin();
+	auto const end = in.end();
+	benchmark::DoNotOptimize(std::addressof(in));
+	benchmark::ClobberMemory();
+	for (auto _ : state) {
+		auto c = Container(begin, end);
+		benchmark::DoNotOptimize(c.data());
+		benchmark::ClobberMemory();
+	}
+}
+
+constexpr std::size_t max_iter_range = 1 << 20;
+
+BENCHMARK_TEMPLATE(benchmark_construct_size, std::vector<unsigned char>)->Arg(5140480);
+BENCHMARK_TEMPLATE(benchmark_construct_size, std_containers::vector<unsigned char>)->Arg(5140480);
+BENCHMARK_TEMPLATE(benchmark_construct_repeat_value, std::vector<unsigned char>, 0)->Arg(5140480);
+BENCHMARK_TEMPLATE(benchmark_construct_repeat_value, std_containers::vector<unsigned char>, 0)->Arg(5140480);
+BENCHMARK_TEMPLATE(benchmark_construct_iterator_pair, std::vector<char>)->Range(0, max_iter_range);
+BENCHMARK_TEMPLATE(benchmark_construct_iterator_pair, std_containers::vector<char>)->Range(0, max_iter_range);
+BENCHMARK_TEMPLATE(benchmark_construct_iterator_pair, std::vector<size_t>)->Range(0, max_iter_range);
+BENCHMARK_TEMPLATE(benchmark_construct_iterator_pair, std_containers::vector<size_t>)->Range(0, max_iter_range);
+BENCHMARK_TEMPLATE(benchmark_construct_iterator_pair, std::vector<std_containers::vector<char>>)->Range(0, max_iter_range);
+BENCHMARK_TEMPLATE(benchmark_construct_iterator_pair, std_containers::vector<std_containers::vector<char>>)->Range(0, max_iter_range);
+BENCHMARK_TEMPLATE(benchmark_construct_iterator_pair, std::vector<std::vector<char>>)->Range(0, max_iter_range);
+BENCHMARK_TEMPLATE(benchmark_construct_iterator_pair, std_containers::vector<std::vector<char>>)->Range(0, max_iter_range);
+
 template<typename Container>
-void benchmark_push_back(benchmark::State& state) {
+void benchmark_push_back(benchmark::State & state) {
 	for (auto _ : state) {
 		auto v = Container();
 		v.push_back(42);
@@ -25,7 +81,7 @@ void benchmark_push_back(benchmark::State& state) {
 }
 
 template<typename Container>
-void benchmark_resize(benchmark::State& state) {
+void benchmark_resize(benchmark::State & state) {
 	for (auto _ : state) {
 		auto v = Container();
 		benchmark::DoNotOptimize(v.data());
@@ -39,16 +95,8 @@ void benchmark_resize(benchmark::State& state) {
 BENCHMARK_TEMPLATE(benchmark_push_back, std::vector<int>);
 BENCHMARK_TEMPLATE(benchmark_push_back, std_containers::vector<int>);
 
-constexpr auto range_max = 10000;
+constexpr auto range_max = 1 << 22;
 BENCHMARK_TEMPLATE(benchmark_resize, std::vector<int>)->Range(0, range_max);
 BENCHMARK_TEMPLATE(benchmark_resize, std_containers::vector<int>)->Range(0, range_max);
 
 } // namespace
-
-int main(int argc, char** argv) {
-	::benchmark::Initialize(&argc, argv);
-	if (::benchmark::ReportUnrecognizedArguments(argc, argv)) {
-		return 1;
-	}
-	::benchmark::RunSpecifiedBenchmarks();
-}
