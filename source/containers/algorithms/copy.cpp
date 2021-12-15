a50b66e688f7367d624f422c1f27219b0a817423
David Stone <davidfromonline@gmail.com>
2021-12-15 11:29:48 -0700

    Add `copy` unit tests

--- source/containers/algorithms/copy.cpp
+++ source/containers/algorithms/copy.cpp
@@ -4,3 +4,48 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/algorithms/copy.hpp>
+
+#include <containers/array.hpp>
+#include <containers/range_view.hpp>
+
+namespace {
+
+using namespace bounded::literal;
+
+static_assert([] {
+	auto container = containers::array{1, 2, 3, 4};
+	containers::copy(
+		containers::range_view(containers::begin(container), containers::begin(container)),
+		containers::begin(container) + 2_bi
+	);
+	return container == containers::array{1, 2, 3, 4};
+}());
+
+static_assert([] {
+	auto container = containers::array{1, 2, 3, 4};
+	containers::copy(
+		containers::range_view(containers::begin(container), containers::begin(container) + 2_bi),
+		containers::begin(container) + 2_bi
+	);
+	return container == containers::array{1, 2, 1, 2};
+}());
+
+static_assert([] {
+	auto container = containers::array{1, 2, 3, 4};
+	containers::copy_backward(
+		containers::range_view(containers::begin(container), containers::begin(container) + 2_bi),
+		containers::end(container)
+	);
+	return container == containers::array{1, 2, 1, 2};
+}());
+
+static_assert([] {
+	auto container = containers::array{1, 2, 3};
+	containers::copy_backward(
+		containers::range_view(containers::begin(container), containers::begin(container) + 2_bi),
+		containers::end(container)
+	);
+	return container == containers::array{1, 1, 2};
+}());
+
+} // namespace
