64c9b41d04e8be3b43b9882fd06128d8568d227b
David Stone <davidfromonline@gmail.com>
2021-12-18 08:18:30 -0700

    Add `find` unit tests

--- source/containers/algorithms/find.cpp
+++ source/containers/algorithms/find.cpp
@@ -4,3 +4,29 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/algorithms/find.hpp>
+
+#include <containers/vector.hpp>
+
+namespace {
+
+using namespace bounded::literal;
+
+constexpr auto test(auto const index, containers::vector<int> const & v) -> bool {
+	auto it = containers::find(v, 5);
+	BOUNDED_ASSERT(index == containers::size(v) or *it == 5);
+	return it - containers::begin(v) == index;
+}
+template<std::size_t size>
+constexpr auto test(auto const index, containers::c_array<int, size> && a) -> bool {
+	return test(index, containers::vector<int>(std::move(a)));
+}
+
+static_assert(test(0_bi, {}));
+static_assert(test(1_bi, {0}));
+static_assert(test(0_bi, {5}));
+static_assert(test(2_bi, {0, 0}));
+static_assert(test(1_bi, {0, 5}));
+static_assert(test(0_bi, {5, 0}));
+static_assert(test(0_bi, {5, 5}));
+
+} // namespace
