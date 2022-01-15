4c4f0d6f5b2d699d9fb7710e7566d60e8979ac42
David Stone <davidfromonline@gmail.com>
2022-01-15 05:05:31 -0700

    Add test for dereference

--- source/containers/dereference.cpp
+++ source/containers/dereference.cpp
@@ -4,3 +4,14 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <containers/dereference.hpp>
+
+#include <containers/array.hpp>
+
+namespace {
+
+static_assert(containers::detail::should_replace_copy_with_move<containers::array<int, 0>>);
+static_assert(containers::detail::should_replace_copy_with_move<containers::array<int, 0> &&>);
+static_assert(!containers::detail::should_replace_copy_with_move<containers::array<int, 0> const &>);
+static_assert(!containers::detail::should_replace_copy_with_move<containers::array<int, 0> &>);
+
+} // namespace
