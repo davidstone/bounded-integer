fc5fdaf818bc917f5a9a430a71596d5d1f8bede1
David Stone <davidfromonline@gmail.com>
2022-03-12 03:58:12 -0700

    `pop_front(c)` can be defined in terms of `c.erase_after(c.before_begin())`

--- include/containers/pop_front.hpp
+++ include/containers/pop_front.hpp
@@ -10,6 +10,7 @@
 #include <containers/front_back.hpp>
 #include <containers/constant_time_erasable.hpp>
 #include <containers/is_range.hpp>
+#include <containers/iterator_t.hpp>
 
 #include <bounded/detail/construct_destroy.hpp>
 #include <bounded/integer.hpp>
@@ -21,7 +22,21 @@ template<typename Container>
 concept member_pop_frontable = requires (Container & container) { container.pop_front(); };
 
 template<typename Container>
-concept pop_frontable = member_pop_frontable<Container> or constant_time_erasable<Container>;
+concept has_member_erase_after = requires (Container & container, iterator_t<Container &> it) {
+	container.erase_after(it);
+};
+
+// `erase_after` is assumed to be constant time
+template<typename Container>
+concept constant_time_erase_afterable =
+	has_member_before_begin<Container> and
+	has_member_erase_after<Container>;
+
+template<typename Container>
+concept pop_frontable =
+	member_pop_frontable<Container> or
+	constant_time_erasable<Container> or
+	constant_time_erase_afterable<Container>;
 
 } // namespace detail
 
@@ -29,6 +44,8 @@ template<detail::pop_frontable Container>
 constexpr auto pop_front(Container & c) -> void {
 	if constexpr (detail::member_pop_frontable<Container>) {
 		c.pop_front();
+	} else if constexpr (detail::constant_time_erase_afterable<Container>) {
+		c.erase_after(c.before_begin());
 	} else {
 		static_assert(constant_time_erasable<Container>);
 		containers::erase(c, containers::begin(c));
