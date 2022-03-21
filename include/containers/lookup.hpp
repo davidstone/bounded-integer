71dd4bfc777db09ee4089dc53de83823ae80f1f2
David Stone <davidfromonline@gmail.com>
2022-03-21 08:20:41 -0600

    Allow brace constructing the key in `lookup`

--- include/containers/lookup.hpp
+++ include/containers/lookup.hpp
@@ -15,7 +15,8 @@
 
 namespace containers {
 
-constexpr auto lookup(associative_range auto && map, auto && key) {
+template<associative_range Map, typename Key = typename std::remove_reference_t<Map>::key_type>
+constexpr auto lookup(Map && map, Key && key) {
 	auto const it = map.find(OPERATORS_FORWARD(key));
 	return it != containers::end(map) ?
 		std::addressof(get_mapped(*it)) :
