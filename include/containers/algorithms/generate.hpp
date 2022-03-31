7a4ed9b1025f4be407708fafaef31abce26cd2cb
David Stone <davidfromonline@gmail.com>
2022-03-31 05:43:46 -0600

    Add `indexed_generate_n`

--- include/containers/algorithms/generate.hpp
+++ include/containers/algorithms/generate.hpp
@@ -5,8 +5,10 @@
 
 #pragma once
 
-#include <containers/common_iterator_functions.hpp>
 #include <containers/begin_end.hpp>
+#include <containers/common_iterator_functions.hpp>
+#include <containers/index_type.hpp>
+#include <containers/offset_type.hpp>
 
 #include <bounded/integer.hpp>
 #include <bounded/unreachable.hpp>
@@ -29,31 +31,31 @@ using namespace bounded::literal;
 
 namespace detail {
 
+template<typename Function>
+using stored_function = std::conditional_t<
+	std::is_empty_v<Function> and std::is_trivially_copyable_v<Function>,
+	std::remove_const_t<Function>,
+	decltype(std::reference_wrapper(std::declval<Function &>()))
+>;
+
 template<typename LHS, typename RHS>
 concept construct_subtractable = requires(LHS const lhs, RHS const rhs) {
 	LHS(lhs - rhs);
 };
 
-} // namespace detail
-
 struct generate_sentinel {};
 
-template<typename Size, typename Function>
+template<typename Offset, typename Function>
 struct generate_n_iterator {
 private:
-	using stored_function = std::conditional_t<
-		std::is_empty_v<Function> and std::is_trivially_copyable_v<Function>,
-		std::remove_const_t<Function>,
-		decltype(std::reference_wrapper(std::declval<Function &>()))
-	>;
-	[[no_unique_address]] Size m_remaining;
-	[[no_unique_address]] stored_function m_generator;
+	[[no_unique_address]] Offset m_remaining;
+	[[no_unique_address]] stored_function<Function> m_generator;
 
 public:
 	using iterator_category = std::input_iterator_tag;
-	using difference_type = decltype(std::declval<Size>() - std::declval<Size>());
+	using difference_type = decltype(std::declval<Offset>() - std::declval<Offset>());
 	
-	constexpr generate_n_iterator(Size const remaining, Function & generator):
+	constexpr generate_n_iterator(Offset const remaining, Function & generator):
 		m_remaining(remaining),
 		m_generator(generator)
 	{
@@ -80,7 +82,7 @@ public:
 	}
 
 	friend constexpr auto operator+(generate_n_iterator it, bounded::constant_t<1>) -> generate_n_iterator {
-		if constexpr (numeric_traits::max_value<Size> == bounded::constant<0>) {
+		if constexpr (numeric_traits::max_value<Offset> == 0_bi) {
 			bounded::unreachable();
 		} else {
 			--it.m_remaining;
@@ -88,6 +90,8 @@ public:
 		}
 	}
 };
+
+} // namespace detail
 
 // TODO: support generators with mutable state
 template<typename Size, typename Function>
@@ -104,13 +108,97 @@ public:
 	}
 
 	constexpr auto begin() const requires std::is_invocable_v<Function const &> {
-		return generate_n_iterator<bounded::integer<0, bounded::builtin_max_value<Size>>, Function const>(m_size, m_generator);
+		return detail::generate_n_iterator(detail::to_offset_type<Size>(m_size), m_generator);
 	}
 	constexpr auto begin() {
-		return generate_n_iterator<bounded::integer<0, bounded::builtin_max_value<Size>>, Function>(m_size, m_generator);
+		return detail::generate_n_iterator(detail::to_offset_type<Size>(m_size), m_generator);
 	}
 	static constexpr auto end() {
-		return generate_sentinel();
+		return detail::generate_sentinel();
+	}
+	constexpr auto size() const {
+		return m_size;
+	}
+};
+
+namespace detail {
+
+template<typename T>
+using generate_index_type = to_index_type<std::conditional_t<
+	std::is_same_v<T, bounded::constant_t<0>>,
+	bounded::constant_t<1>,
+	T
+>>;
+
+template<typename Function, typename Size>
+concept index_invocable = std::is_invocable_v<Function const &, generate_index_type<Size>>;
+
+template<typename Offset, typename Function>
+struct indexed_generate_n_iterator {
+private:
+	[[no_unique_address]] Offset m_offset;
+	[[no_unique_address]] stored_function<Function const> m_generator;
+
+public:
+	using difference_type = decltype(std::declval<Offset>() - std::declval<Offset>());
+	
+	constexpr indexed_generate_n_iterator(Offset const remaining, Function const & generator):
+		m_offset(remaining),
+		m_generator(generator)
+	{
+	}
+
+	constexpr decltype(auto) operator*() const {
+		return std::invoke(m_generator, bounded::assume_in_range<generate_index_type<Offset>>(m_offset));
+	}
+	OPERATORS_ARROW_DEFINITIONS
+
+	friend constexpr auto operator<=>(indexed_generate_n_iterator const lhs, indexed_generate_n_iterator const rhs) {
+		return lhs.m_offset <=> rhs.m_offset;
+	}
+	friend constexpr auto operator==(indexed_generate_n_iterator const lhs, indexed_generate_n_iterator const rhs) -> bool {
+		return lhs.m_offset == rhs.m_offset;
+	}
+
+	friend constexpr auto operator+(indexed_generate_n_iterator const it, difference_type const difference) -> indexed_generate_n_iterator {
+		return {bounded::assume_in_range<Offset>(it.m_offset + difference), it.m_generator};
+	}
+	friend constexpr auto operator+(indexed_generate_n_iterator const it, bounded::constant_t<1>) -> indexed_generate_n_iterator requires(bounded::detail::builtin_integer<Offset> or numeric_traits::max_value<Offset> == 0_bi) {
+		if constexpr (bounded::detail::builtin_integer<Offset>) {
+			return it + 1;
+		} else {
+			bounded::unreachable();
+		}
+	}
+	friend constexpr auto operator+(indexed_generate_n_iterator const it, bounded::constant_t<-1>) -> indexed_generate_n_iterator requires(bounded::detail::builtin_integer<Offset> or numeric_traits::max_value<Offset> == 0_bi) {
+		if constexpr (bounded::detail::builtin_integer<Offset>) {
+			return it - 1;
+		} else {
+			bounded::unreachable();
+		}
+	}
+
+	friend constexpr auto operator-(indexed_generate_n_iterator const lhs, indexed_generate_n_iterator const rhs) -> difference_type {
+		return lhs.m_offset - rhs.m_offset;
+	}
+};
+
+} // namespace detail
+
+template<typename Size, detail::index_invocable<Size> Function>
+struct indexed_generate_n {
+private:
+	[[no_unique_address]] Size m_size;
+	[[no_unique_address]] Function m_generator;
+public:
+	constexpr indexed_generate_n(Size const size, Function generator):
+		m_size(size),
+		m_generator(std::move(generator))
+	{
+	}
+
+	constexpr auto begin() const {
+		return detail::indexed_generate_n_iterator(detail::to_offset_type<Size>(0_bi), m_generator);
 	}
 	constexpr auto size() const {
 		return m_size;
--- source/containers/algorithms/generate.cpp
+++ source/containers/algorithms/generate.cpp
@@ -18,4 +18,16 @@ static_assert(containers::equal(containers::generate_n(0_bi, bounded::value_to_f
 static_assert(containers::equal(containers::generate_n(1_bi, bounded::value_to_function(5)), containers::array{5}));
 static_assert(containers::equal(containers::generate_n(2_bi, bounded::value_to_function(5)), containers::array{5, 5}));
 
+constexpr auto identity = [](auto const value) {
+	return value;
+};
+
+static_assert(containers::equal(containers::indexed_generate_n(0_bi, identity), containers::array<bounded::constant_t<0>, 0_bi>{}));
+static_assert(containers::equal(containers::indexed_generate_n(1_bi, identity), containers::array{0_bi}));
+static_assert(containers::equal(containers::indexed_generate_n(2_bi, identity), containers::array{0_bi, 1_bi}));
+
+static_assert(containers::equal(containers::indexed_generate_n(0, identity), containers::array<int, 0_bi>{}));
+static_assert(containers::equal(containers::indexed_generate_n(1, identity), containers::array{0}));
+static_assert(containers::equal(containers::indexed_generate_n(2, identity), containers::array{0, 1}));
+
 } // namespace
