0b4b622c278b51b30ebb9c4a2ecda2fb90292e72
David Stone <davidfromonline@gmail.com>
2022-12-04 13:23:07 -0700

    Directly test `bidirectional_linked_list::lazy_push_back`

--- source/containers/bidirectional_linked_list.cpp
+++ source/containers/bidirectional_linked_list.cpp
@@ -15,7 +15,35 @@ static_assert(bounded::convertible_to<containers::bidirectional_linked_list<int>
 static_assert(containers_test::test_sequence_container<containers::bidirectional_linked_list<int>>());
 static_assert(containers_test::test_sequence_container<containers::bidirectional_linked_list<bounded_test::integer>>());
 
-// TODO: Directly test `splice`, `pop_back`, and `lazy_push_back`
+template<typename Integer>
+constexpr auto test_lazy_push_back_empty() {
+	auto list = containers::bidirectional_linked_list<Integer>();
+	auto const & element = list.lazy_push_back(bounded::construct<Integer>);
+	BOUNDED_TEST(containers::equal(list, containers::array{0}));
+	BOUNDED_TEST(std::addressof(element) == std::addressof(containers::front(list)));
+}
+
+template<typename Integer>
+constexpr auto test_lazy_push_back_non_empty() {
+	auto list = containers::bidirectional_linked_list<Integer>({1});
+	auto const ptr = std::addressof(containers::front(list));
+	auto const & element = list.lazy_push_back(bounded::construct<Integer>);
+	BOUNDED_TEST(containers::equal(list, containers::array{1, 0}));
+	BOUNDED_TEST(std::addressof(element) == std::addressof(containers::back(list)));
+	BOUNDED_TEST(ptr == std::addressof(containers::front(list)));
+}
+
+template<typename Integer>
+constexpr auto test_lazy_push_back() {
+	test_lazy_push_back_empty<Integer>();
+	test_lazy_push_back_non_empty<Integer>();
+	return true;
+}
+
+static_assert(test_lazy_push_back<int>());
+static_assert(test_lazy_push_back<bounded_test::non_copyable_integer>());
+
+// TODO: Directly test `splice`, `pop_back`
 
 struct recursive {
 	containers::bidirectional_linked_list<recursive> m;
