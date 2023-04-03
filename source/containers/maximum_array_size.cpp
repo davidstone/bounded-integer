3221446b67c9d0ff908eff253261c9d9d863a290
David Stone <davidfromonline@gmail.com>
2023-04-03 13:31:24 -0600

    Reduce value of `maximum_array_size` to something more realistic.

--- source/containers/maximum_array_size.cpp
+++ source/containers/maximum_array_size.cpp
@@ -10,10 +10,15 @@ import numeric_traits;
 import std_module;
 
 namespace containers {
+using namespace bounded::literal;
 
-// TODO: Should this be limited to 2^52 - 1 bytes?
+constexpr auto maximum_array_bytes = bounded::min(
+	bounded::constant<numeric_traits::max_value<std::ptrdiff_t>>,
+	(1_bi << 52_bi) - 1_bi
+);
+
 export template<typename T>
-constexpr auto maximum_array_size = bounded::normalize<numeric_traits::max_value<std::ptrdiff_t> / sizeof(T)>;
+constexpr auto maximum_array_size = bounded::normalize<maximum_array_bytes / bounded::size_of<T>>;
 
 export template<typename T>
 using array_size_type = bounded::integer<0, maximum_array_size<T>>;
