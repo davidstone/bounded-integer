acc24897f89bcb12cad5aa805d2da9acfe5e697a
David Stone <davidfromonline@gmail.com>
2024-01-01 18:20:01 -0700

    `remove_none` is a `forward_range` if the input container is a `forward_range` and the value type is sufficiently trivial.

--- source/containers/algorithms/remove_none.cpp
+++ source/containers/algorithms/remove_none.cpp
@@ -13,10 +13,12 @@ import containers.algorithms.compare;
 import containers.array;
 import containers.begin_end;
 import containers.dereference;
+import containers.forward_range;
 import containers.iter_difference_t;
 import containers.iterator_t;
 import containers.range;
 import containers.range_reference_t;
+import containers.sentinel_t;
 
 import bounded;
 import tv;
@@ -25,76 +27,172 @@ import std_module;
 namespace containers {
 
 template<typename Range>
+concept use_inplace =
+	bounded::mostly_trivial<tv::optional<containers::range_reference_t<Range>>>;
+
+template<typename Range>
 struct remove_none_data {
 	[[no_unique_address]] Range range;
-	tv::optional<range_reference_t<Range>> cached_value;
+	[[no_unique_address]] tv::optional<range_reference_t<Range>> cached_value;
+};
+
+template<typename Range>
+struct iterator_reference_data {
+	constexpr iterator_reference_data(iterator_t<Range> it, remove_none_data<Range> & data):
+		m_it(std::move(it)),
+		m_data(data)
+	{
+	}
+
+	iterator_reference_data(iterator_reference_data &&) = default;
+	iterator_reference_data(iterator_reference_data const &) = delete;
+	auto operator=(iterator_reference_data &&) & -> iterator_reference_data & = default;
+	auto operator=(iterator_reference_data const &) & -> iterator_reference_data & = delete;
+
+	constexpr auto iterator() const -> iterator_t<Range> const & {
+		return m_it;
+	}
+	constexpr auto iterator() -> iterator_t<Range> & {
+		return m_it;
+	}
+	constexpr auto sentinel() const -> sentinel_t<Range> {
+		return containers::end(m_data.get().range);
+	}
+
+	constexpr auto cached() const -> tv::optional<range_reference_t<Range>> & {
+		return m_data.get().cached_value;
+	}
+private:
+	[[no_unique_address]] iterator_t<Range> m_it;
+	std::reference_wrapper<remove_none_data<Range>> m_data;
+};
+
+template<typename Range>
+struct iterator_inplace_data {
+	constexpr iterator_inplace_data(iterator_t<Range> it, sentinel_t<Range> last):
+		m_it(std::move(it)),
+		m_last(std::move(last))
+	{
+	}
+
+	constexpr auto iterator() const -> iterator_t<Range> const & {
+		return m_it;
+	}
+	constexpr auto iterator() -> iterator_t<Range> & {
+		return m_it;
+	}
+	constexpr auto sentinel() const -> sentinel_t<Range> {
+		return m_last;
+	}
+
+	constexpr auto cached() const -> tv::optional<range_reference_t<Range>> const & {
+		return m_cached_value;
+	}
+	constexpr auto cached() -> tv::optional<range_reference_t<Range>> & {
+		return m_cached_value;
+	}
+private:
+	[[no_unique_address]] iterator_t<Range> m_it;
+	[[no_unique_address]] sentinel_t<Range> m_last;
+	[[no_unique_address]] tv::optional<range_reference_t<Range>> m_cached_value;
 };
 
 template<typename Range>
 struct remove_none_iterator {
 	using difference_type = iter_difference_t<iterator_t<Range>>;
 
-	constexpr explicit remove_none_iterator(iterator_t<Range> it, remove_none_data<Range> & data):
-		m_it(std::move(it)),
-		m_data(data)
+	constexpr explicit remove_none_iterator(auto && ... args):
+		m_data(OPERATORS_FORWARD(args)...)
 	{
 		advance_to_first_value();
 	}
 
-	remove_none_iterator(remove_none_iterator &&) = default;
-	remove_none_iterator(remove_none_iterator const &) = delete;
-	auto operator=(remove_none_iterator &&) & -> remove_none_iterator & = default;
-	auto operator=(remove_none_iterator const &) & -> remove_none_iterator & = delete;
-
 	constexpr auto operator*() const -> auto && {
-		return **std::move(m_data.get().cached_value);
+		if constexpr (use_inplace<Range>) {
+			return **m_data.cached();
+		} else {
+			return **std::move(m_data.cached());
+		}
 	}
+
 	friend constexpr auto operator+(remove_none_iterator lhs, bounded::constant_t<1>) -> remove_none_iterator {
-		++lhs.m_it;
+		++lhs.m_data.iterator();
 		lhs.advance_to_first_value();
 		return lhs;
 	}
 
 	friend constexpr auto operator==(remove_none_iterator const & lhs, remove_none_iterator const & rhs) -> bool requires bounded::equality_comparable<iterator_t<Range>> {
-		return lhs.m_it == rhs.m_it;
+		return lhs.m_data.iterator() == rhs.m_data.iterator();
 	}
 	friend constexpr auto operator==(remove_none_iterator const & lhs, std::default_sentinel_t) -> bool {
-		return lhs.m_it == containers::end(lhs.m_data.get().range);
+		return lhs.m_data.iterator() == lhs.m_data.sentinel();
 	}
 
 private:
 	constexpr auto advance_to_first_value() -> void {
-		auto const last = containers::end(m_data.get().range);
-		while (m_it != last) {
-			auto & storage = m_data.get().cached_value;
-			storage.emplace([&] -> decltype(auto) { return dereference<Range>(m_it); });
+		auto const last = m_data.sentinel();
+		auto & it = m_data.iterator();
+		while (it != last) {
+			auto & storage = m_data.cached();
+			storage.emplace([&] -> decltype(auto) { return dereference<Range>(it); });
 			if (*storage) {
 				break;
 			}
-			++m_it;
+			++it;
 		}
 	}
 
-	[[no_unique_address]] iterator_t<Range> m_it;
-	std::reference_wrapper<remove_none_data<Range>> m_data;
+	using data_t = std::conditional_t<
+		use_inplace<Range>,
+		iterator_inplace_data<Range>,
+		iterator_reference_data<Range>
+	>;
+	[[no_unique_address]] data_t m_data;
 };
 
 // This goes from range<optional<T>> to range<T>
 export template<range Range>
 struct remove_none {
 	constexpr explicit remove_none(Range && input):
-		m_data(OPERATORS_FORWARD(input), tv::none)
+		m_data(OPERATORS_FORWARD(input))
 	{
 	}
-	constexpr auto begin() {
-		return remove_none_iterator<Range>(containers::begin(m_data.range), m_data);
+	constexpr auto begin() && -> remove_none_iterator<Range> {
+		if constexpr (use_inplace<Range>) {
+			return remove_none_iterator<Range>(
+				containers::begin(OPERATORS_FORWARD(m_data)),
+				containers::end(OPERATORS_FORWARD(m_data))
+			);
+		} else {
+			return remove_none_iterator<Range>(
+				containers::begin(std::move(m_data).range),
+				m_data
+			);
+		}
+	}
+	constexpr auto begin() const & -> remove_none_iterator<Range> requires use_inplace<Range> and forward_range<Range> {
+		return remove_none_iterator<Range>(
+			containers::begin(m_data),
+			containers::end(m_data)
+		);
+	}
+	constexpr auto begin() & -> remove_none_iterator<Range> requires use_inplace<Range> and forward_range<Range> {
+		return remove_none_iterator<Range>(
+			containers::begin(m_data),
+			containers::end(m_data)
+		);
 	}
 	static constexpr auto end() {
 		return std::default_sentinel;
 	}
 
 private:
-	remove_none_data<Range> m_data;
+	using data_t = std::conditional_t<
+		use_inplace<Range>,
+		Range,
+		remove_none_data<Range>
+	>;
+	[[no_unique_address]] data_t m_data;
 };
 
 template<typename Range>
