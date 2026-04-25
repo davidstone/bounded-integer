58b59ecac2f088000454e47dd2706e7202d141fa
David Stone <davidfromonline@gmail.com>
2026-04-25 15:58:36 -0600

    Move tests for `ordered_associative_container` to their own module

--- source/containers/ordered_associative_container.cpp
+++ source/containers/ordered_associative_container.cpp
@@ -6,14 +6,6 @@
 export module containers.ordered_associative_container;
 
 import containers.associative_container;
-import containers.array;
-import containers.c_array;
-import containers.flat_map;
-import containers.string;
-import containers.vector;
-
-import bounded;
-import std_module;
 
 namespace containers {
 
@@ -36,35 +28,3 @@ export template<typename T>
 concept ordered_associative_container = ordered_associative_range<T> and associative_container<T>;
 
 } // namespace containers
-
-using namespace bounded::literal;
-
-template<typename... Ts>
-constexpr auto all_qualifications_are_ordered_associative_helper = (... and containers::ordered_associative_range<Ts>);
-
-template<typename T>
-constexpr auto all_qualifications_are_ordered_associative = all_qualifications_are_ordered_associative_helper<T, T const, T &, T const &, T &&>;
-
-template<typename... Ts>
-constexpr auto no_qualifications_are_ordered_associative_helper = (... and !containers::ordered_associative_range<Ts>);
-
-template<typename T>
-constexpr auto no_qualifications_are_ordered_associative = no_qualifications_are_ordered_associative_helper<T, T const, T &, T const &, T &&>;
-
-static_assert(no_qualifications_are_ordered_associative<int>);
-static_assert(no_qualifications_are_ordered_associative<int *>);
-
-static_assert(no_qualifications_are_ordered_associative<containers::c_array<int, 5>>);
-
-static_assert(no_qualifications_are_ordered_associative<containers::array<int, 5_bi>>);
-static_assert(no_qualifications_are_ordered_associative<containers::array<int, 0_bi>>);
-static_assert(no_qualifications_are_ordered_associative<containers::vector<int>>);
-static_assert(no_qualifications_are_ordered_associative<containers::string>);
-
-static_assert(all_qualifications_are_ordered_associative<containers::flat_map<int, int>>);
-
-static_assert(no_qualifications_are_ordered_associative<std::array<int, 5>>);
-static_assert(no_qualifications_are_ordered_associative<std::array<int, 0>>);
-static_assert(no_qualifications_are_ordered_associative<std::vector<int>>);
-static_assert(all_qualifications_are_ordered_associative<std::map<int, int>>);
-static_assert(no_qualifications_are_ordered_associative<std::unordered_map<int, int>>);
---
+++ source/containers/test/ordered_associative_container.cpp
@@ -0,0 +1,48 @@
+// Copyright David Stone 2020.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+export module containers.test.ordered_associative_container;
+
+import containers.array;
+import containers.c_array;
+import containers.flat_map;
+import containers.ordered_associative_container;
+import containers.string;
+import containers.vector;
+
+import bounded;
+import std_module;
+
+using namespace bounded::literal;
+
+template<typename... Ts>
+constexpr auto all_qualifications_are_ordered_associative_helper = (... and containers::ordered_associative_range<Ts>);
+
+template<typename T>
+constexpr auto all_qualifications_are_ordered_associative = all_qualifications_are_ordered_associative_helper<T, T const, T &, T const &, T &&>;
+
+template<typename... Ts>
+constexpr auto no_qualifications_are_ordered_associative_helper = (... and !containers::ordered_associative_range<Ts>);
+
+template<typename T>
+constexpr auto no_qualifications_are_ordered_associative = no_qualifications_are_ordered_associative_helper<T, T const, T &, T const &, T &&>;
+
+static_assert(no_qualifications_are_ordered_associative<int>);
+static_assert(no_qualifications_are_ordered_associative<int *>);
+
+static_assert(no_qualifications_are_ordered_associative<containers::c_array<int, 5>>);
+
+static_assert(no_qualifications_are_ordered_associative<containers::array<int, 5_bi>>);
+static_assert(no_qualifications_are_ordered_associative<containers::array<int, 0_bi>>);
+static_assert(no_qualifications_are_ordered_associative<containers::vector<int>>);
+static_assert(no_qualifications_are_ordered_associative<containers::string>);
+
+static_assert(all_qualifications_are_ordered_associative<containers::flat_map<int, int>>);
+
+static_assert(no_qualifications_are_ordered_associative<std::array<int, 5>>);
+static_assert(no_qualifications_are_ordered_associative<std::array<int, 0>>);
+static_assert(no_qualifications_are_ordered_associative<std::vector<int>>);
+static_assert(all_qualifications_are_ordered_associative<std::map<int, int>>);
+static_assert(no_qualifications_are_ordered_associative<std::unordered_map<int, int>>);
